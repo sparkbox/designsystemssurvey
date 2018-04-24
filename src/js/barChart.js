c3a86e29f864480478e9b6a3e0b6bd1f59d8d826
Katy Bowman <katy@heysparkbox.com>
2018-04-24 15:27:51 -0400

    refactor: change class names and refactor js for bar charts

--- src/js/barChart.js
+++ src/js/barChart.js
@@ -18,26 +18,27 @@ const getBarChartTableData = table => {
 const createBarChart = (tableData, chartSize) => {
   let chart = '';
   tableData.forEach(dataObject => {
-    const barWidth = dataObject.value / chartSize * 100;
+    const barWidth = `${dataObject.value / chartSize * 100}%`;
     chart += `
-      <div class="entry">
-        <div class="entry__category">${dataObject.label}</div>
-        <div class="entry__bar" style="width: ${barWidth}">
-          <div class="entry__value">${dataObject.value}%</div>
+      <div class="cmp-chart__entry">
+        <div class="cmp-chart__category">${dataObject.label}</div>
+        <div class="cmp-chart__bar" style="width: ${barWidth}">
+          <div class="cmp-chart__value">${dataObject.value}%</div>
         </div>
       </div>
     `;
   });
-  console.log(chart);
   return chart;
 };
 
-const addBarChart = barChartElement => {
-  const tableData = getBarChartTableData(barChartElement);
-  const chartSize = barChartElement.getAttribute('data-size');
-  const barChart = createBarChart(tableData, chartSize);
-  barChartElement.innerHTML += barChart;
-  barChartElement.getElementsByTagName('table')[0].style.display = 'none';
+const addBarCharts = (barChartElements) => {
+  barChartElements.forEach(barChartElement => {
+    const tableData = getBarChartTableData(barChartElement);
+    const chartSize = barChartElement.getAttribute('data-size');
+    const barChart = createBarChart(tableData, chartSize);
+    barChartElement.innerHTML += barChart;
+    barChartElement.getElementsByTagName('table')[0].style.display = 'none';
+  });
 };
 
-export default addBarChart;
+export default addBarCharts;
--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,21 @@
 'use strict';
 
-import addBarChart from './barChart.js';
+import addBarCharts from './barChart.js';
 
 const getTableElements = chartType => {
   let tableClass = '';
   if (chartType === 'barChart') {
-    tableClass = '.chart--type-bar';
+    tableClass = '.cmp-chart--bar';
   } else if (chartType === 'stackedBarChart') {
-    tableClass = '.chart--type-stacked-bar';
+    tableClass = '.cmp-chart--stacked-bar';
   }
   return Array.from(document.querySelectorAll(tableClass));
-};
-
-const forEachNode = function(array, callback, scope) {
-  for (var i = 0; i < array.length; i++) {
-    callback.call(scope, i, array[i]);
-  }
 };
 
 const addCharts = () => {
   const barChartElements = getTableElements('barChart');
   const stackedBarChartElements = getTableElements('stackedBarChart');
-  forEachNode(barChartElements, function(index, value) {
-    addBarChart(value);
-  });
-};
+  addBarCharts(barChartElements);
+}
 
 addCharts();
