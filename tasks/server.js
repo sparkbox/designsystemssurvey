13af1d1da1dd191bc2ef478bbd1ccb79911659ef
Ryan Cromwell <ryan@heysparkbox.com>
2018-04-25 10:10:56 -0400

    :art: style (Server): Fix indenting of the Basic auth conditional

--- tasks/server.js
+++ tasks/server.js
@@ -9,12 +9,12 @@ if (process.env.ENFORCE_SSL) {
 }
 
 // Basic Auth
-  if (process.env.NODE_ENV === 'production') {
-    const auth = require('http-auth');
-    const basic = auth.basic({
-      realm: "Sparkbox Design Systems Survey",
-      file: __dirname + "/../.htpasswd"
-    });
+if (process.env.NODE_ENV === 'production') {
+  const auth = require('http-auth');
+  const basic = auth.basic({
+    realm: "Sparkbox Design Systems Survey",
+    file: __dirname + "/../.htpasswd"
+  });
 
   app.use(auth.connect(basic));
 }
