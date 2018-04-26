0c3489fef3d43cbb0ad79a75bb4c8170ed248ea3
Ryan Cromwell <ryan@heysparkbox.com>
2018-04-25 22:51:28 -0400

    feat (Server): Enable gzip for all content

--- tasks/server.js
+++ tasks/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const enforceSSL = require('express-enforces-ssl');
+const compression = require('compression');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,8 @@ if (process.env.ENFORCE_SSL) {
   app.enable('trust proxy');
   app.use(enforceSSL());
 }
+
+app.use(compression()); //gzip
 
 // Basic Auth
 if (process.env.NODE_ENV === 'production') {
