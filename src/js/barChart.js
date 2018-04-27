e3d33e7366743437b0740e1883ab25031bc7e526
zastrow <philip@zastrow.co>
2018-04-27 11:15:19 -0400

    feat: Update charts for real content and small screens

--- src/js/barChart.js
+++ src/js/barChart.js
@@ -17,29 +17,40 @@ const getBarChartTableData = (table) => {
   return tableData;
 };
 
-const createBarChart = (tableData, chartSize) => {
-  let chart = '';
-  tableData.forEach(dataObject => {
+const createBarChart = (tableData, chartSize, chartTheme) => {
+  let chart = 
+  `<div class="cmp-chart cmp-chart--${chartTheme}">
+    <div class="cmp-chart__range" aria-hidden="true">
+      <div class="cmp-chart__low">0%</div>
+      <div class="cmp-chart__high">${chartSize}%</div>
+    </div>`
+  ;
+
+  for (let i = 0; i < tableData.length; i++ ) {
+    const dataObject = tableData[i];
     const barWidth = `${dataObject.value / chartSize * 100}%`;
     chart += `
-      <div class="cmp-chart__entry">
+      <div class="cmp-chart__entry cmp-chart__entry--${i + 1}">
         <div class="cmp-chart__category">${dataObject.label}</div>
         <div class="cmp-chart__bar" style="width: ${barWidth}">
           <div class="cmp-chart__value">${dataObject.value}%</div>
         </div>
       </div>
-    `;
-  });
+    `
+  }
+  chart += `</div>`;
   return chart;
 };
 
 const addBarCharts = (barChartElements) => {
   barChartElements.forEach(chartElement => {
+    const tableParent = chartElement.parentNode;
     const tableData = getBarChartTableData(chartElement);
     const chartSize = chartElement.getAttribute('data-size');
-    const barChart = createBarChart(tableData, chartSize);
-    chartElement.innerHTML += barChart;
-    chartElement.getElementsByTagName('table')[0].style.display = 'none';
+    const chartTheme = chartElement.getAttribute('data-theme');
+    const barChart = createBarChart(tableData, chartSize, chartTheme);
+    tableParent.innerHTML += barChart;
+    tableParent.getElementsByTagName('table')[0].style.display = 'none';
   });
 };
 
