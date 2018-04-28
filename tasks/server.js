f61b6b41e094f971b8b7c16a408952d53ae30f41
Katy Bowman <katy@heysparkbox.com>
2018-04-27 21:57:33 -0400

    chore: redirect 404 to homepage

--- tasks/server.js
+++ tasks/server.js
@@ -16,6 +16,11 @@ app.use(compression()); //gzip
 // Basic Auth
 app.use('/', auth, express.static('dist'));
 
+//404 redirects to home page
+app.use(function (req, res, next) {
+  res.status(404).redirect('/');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
