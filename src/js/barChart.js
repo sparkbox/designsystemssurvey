de5495842f2168266abadd1ef3509bf5e53d674d
zastrow <philip@zastrow.co>
2018-04-30 11:40:13 -0400

    fix: Remove chartSize from JS and HBS files

--- src/js/barChart.js
+++ src/js/barChart.js
@@ -17,18 +17,18 @@ const getBarChartTableData = (table) => {
   return tableData;
 };
 
-const createBarChart = (tableData, chartSize, chartTheme) => {
+const createBarChart = (tableData, chartTheme) => {
   let chart = 
   `<div class="cmp-chart cmp-chart--${chartTheme}">
     <div class="cmp-chart__range" aria-hidden="true">
       <div class="cmp-chart__low">0%</div>
-      <div class="cmp-chart__high">${chartSize}%</div>
+      <div class="cmp-chart__high">100%</div>
     </div>`
   ;
 
   for (let i = 0; i < tableData.length; i++ ) {
     const dataObject = tableData[i];
-    const barWidth = `${dataObject.value / chartSize * 100}%`;
+    const barWidth = `${dataObject.value}%`;
     chart += `
       <div class="cmp-chart__entry cmp-chart__entry--${i + 1}">
         <div class="cmp-chart__category">${dataObject.label}</div>
@@ -46,9 +46,8 @@ const addBarCharts = (barChartElements) => {
   barChartElements.forEach(chartElement => {
     const tableParent = chartElement.parentNode;
     const tableData = getBarChartTableData(chartElement);
-    const chartSize = chartElement.getAttribute('data-size');
     const chartTheme = chartElement.getAttribute('data-theme');
-    const barChart = createBarChart(tableData, chartSize, chartTheme);
+    const barChart = createBarChart(tableData, chartTheme);
     tableParent.innerHTML += barChart;
     tableParent.getElementsByTagName('table')[0].style.display = 'none';
   });
