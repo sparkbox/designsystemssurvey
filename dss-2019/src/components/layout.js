b998efcd5db752949480f7fb5776c67f77646dda
Melissa Thompson <mthompson@heysparkbox.com>
2019-05-21 10:28:48 -0400

    setup: add node sass

--- dss-2019/src/components/layout.js
+++ dss-2019/src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "../scss/base.scss"
 
 const Layout = ({ children }) => (
   <StaticQuery
