405ac98f25fcd594cd6f16c383b07c74f1a439ed
Heather Taylor <heather@heysparkbox.com>
2019-05-24 20:19:12 -0400

    feat: add list styles
    JIRA: DDS-48

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,6 +9,25 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+
+    <h2>List Elements</h2>
+    <p>Unordered List</p>
+    <ul>
+      <li>One</li>
+      <li>Two</li>
+      <li>Three</li>
+      <li>Four</li>
+      <li>Five</li>
+    </ul>
+    <p>Ordered List</p>
+    <ol>
+      <li>One</li>
+      <li>Two</li>
+      <li>Three</li>
+      <li>Four</li>
+      <li>Five</li>
+    </ol>
+
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
