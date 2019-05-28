6d1f19198d27ad5d26f85aef0cf5d649a0162077
Melissa Thompson <mthompson@heysparkbox.com>
2019-05-28 12:30:31 -0400

    feat: tweak styles, add text styles to index

--- src/components/layout.js
+++ src/components/layout.js
@@ -34,7 +34,7 @@ const Layout = ({ children }) => (
         <div
           style={{
             margin: `0 auto`,
-            maxWidth: 960,
+            maxWidth: 1400,
             padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
           }}
