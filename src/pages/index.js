6df4916483fb3343f7066fe7cb61ce461e6bbc6f
Melissa Thompson <mthompson@heysparkbox.com>
2019-05-28 13:04:39 -0400

    feat: add callout text style

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ const IndexPage = () => (
     <h2 class="headline-1--small"><span data-text="Table of Contents">Table of Contents</span></h2>
     <p class="headline-4" style={{background: `black`,}}>The Respondents</p>
     <h3 class="headline-2">The Respondents</h3>
+    <h4 class="headline-3"><span data-text="The 2019 Design Systems Survey received 148 responses from in-house teams who use a design system for their organizations.">The 2019 Design Systems Survey received 148 responses from in-house teams who use a design system for their organizations.</span></h4>
     <p class="body-copy" style={{background: `black`,}}>This survey had 256 responses comprised of two respondent types:</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
