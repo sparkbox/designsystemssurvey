109d09c4c670fcc290f70daf3e80bb5639579f92
Melissa Thompson <mthompson@heysparkbox.com>
2019-05-28 12:30:31 -0400

    feat: tweak styles, add text styles to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1 class="headline-1"><span data-text="design systems">Design systems</span></h1>
+    <p class="accent-font"><span data-text="survey">Survey</span></p>
+    <h2 class="headline-1--small"><span data-text="Table of Contents">Table of Contents</span></h2>
+    <p class="headline-4" style={{background: `black`,}}>The Respondents</p>
+    <h3 class="headline-2">The Respondents</h3>
+    <p class="body-copy" style={{background: `black`,}}>This survey had 256 responses comprised of two respondent types:</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
