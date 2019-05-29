28adeedfe012583813e6d4ce1fb1fb65f114b9c2
Heather Taylor <heather@heysparkbox.com>
2019-05-28 22:38:31 -0400

    feat: add into copy
    https://sparkbox.atlassian.net/browse/DSS-41

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1 class="headline-1"><span data-text="design systems">Design systems</span></h1>
+    <div className="headline-5">
+      <p>The 2019 Design Systems Survey is intended to illuminate how design systems are created, used, and maintained. Design Systems have the ability to solve real-life problems, establishing consistency in design and code, promoting efficiency when creating products, communicating usage guidance, encouraging reuse instead of rework, increasing the accessibility and usability of digital properties, and so much more.</p>
+      <p>For the <Link to="#">second year in a row</Link>, <Link to="https://seesparkbox.com/">Sparkbox</Link>, a web design and development firm, has conducted this survey to learn more about both the challenges and benefits of building and using design systems. By analyzing the reflections of in-house teams who use a design system and agencies who’ve helped organizations build new or maintain existing design systems, we look to inform those who want to build or improve their own.</p>
+    </div>
     <Link to="/page-2/">Go to typography and basic body copy styles on page 2</Link>
   </Layout>
 )
