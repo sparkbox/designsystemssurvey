c1d9bf8fbc49af9414c52ad57bfe51d99cf148a5
Melissa Thompson <mthompson@heysparkbox.com>
2019-05-30 14:03:34 -0400

    refactor: existing text style names

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,17 +10,17 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
 
-    <h1 class="headline-1"><span data-text="design systems">Design systems</span></h1>
+    <h1 class="chrome"><span data-text="design systems">Design systems</span></h1>
     <p class="accent-font"><span data-text="survey">Survey</span></p>
-    <h2 class="headline-1--small"><span data-text="Table of Contents">Table of Contents</span></h2>
-    <p class="headline-4">The Respondents</p>
-    <h3 class="headline-2">The Respondents</h3>
-    <h4 class="headline-3"><span data-text="The 2019 Design Systems Survey received 148 responses from in-house teams who use a design system for their organizations.">The 2019 Design Systems Survey received 148 responses from in-house teams who use a design system for their organizations.</span></h4>
-    <p class="diagram-headline util-margin-btm0">In-House Respondents Breakdown</p>
+    <h2 class="chrome--small"><span data-text="Table of Contents">Table of Contents</span></h2>
+    <p class="label-heading">The Respondents</p>
+    <h3 class="section-heading">The Respondents</h3>
+    <h4 class="primary-content-heading"><span data-text="The 2019 Design Systems Survey received 148 responses from in-house teams who use a design system for their organizations.">The 2019 Design Systems Survey received 148 responses from in-house teams who use a design system for their organizations.</span></h4>
+    <p class="diagram-heading util-margin-btm0">In-House Respondents Breakdown</p>
     <p class="diagram-copy util-margin-top0">Respondents could select multiple from 19 options and fill out a response to “other”, 163 in-house and agency respondents</p>
     <p class="body-copy">This survey had 256 responses comprised of two respondent types:</p>
 
-    <h2 className="headline-4">List Elements</h2>
+    <h2 className="label-heading">List Elements</h2>
     <p className="body-copy">Unordered List</p>
     <ul>
       <li>One</li>
