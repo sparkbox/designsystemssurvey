f3a25d2e1442934d1cdb2a8e7cda82cebd057d68
Melissa Thompson <mthompson@heysparkbox.com>
2019-05-30 09:22:25 -0400

    feat: toc component
    * add toc to base.scss
    * remove .scss extension from other imports in base.scss
    * create toc component
    * add component to index

---
+++ src/components/table-of-contents.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const TableOfContents = () => (
+  <div class="cmp-table-of-contents">
+    <h2 class="font-chrome font-chrome--small">Table of Contents</h2>
+    <div class="cmp-table-of-contents__items">
+      <ol class="cmp-table-of-contents__list">
+        <li class="cmp-table-of-contents__item"><a href="#" class="font-label cmp-table-of-contents__link">The Respondents</a></li>
+        <li class="cmp-table-of-contents__item"><a href="#" class="font-label cmp-table-of-contents__link">Design System Contents and Functionality</a></li>
+        <li class="cmp-table-of-contents__item"><a href="#" class="font-label cmp-table-of-contents__link">Values and Challenges of Design Systems</a></li>
+      </ol>
+      <ol class="cmp-table-of-contents__list cmp-table-of-contents__list--start4">
+        <li class="cmp-table-of-contents__item"><a href="#" class="font-label cmp-table-of-contents__link">Creating A Successful Design System</a></li>
+        <li class="cmp-table-of-contents__item"><a href="#" class="font-label cmp-table-of-contents__link">Learn More about Design Systems</a></li>
+      </ol>
+    </div>
+  </div>
+)
+
+export default TableOfContents
