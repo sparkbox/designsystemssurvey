c7f29e107f735094848ad994b9fdb5ad9791cbbc
Melissa Thompson <mthompson@heysparkbox.com>
2019-05-30 09:22:25 -0400

    feat: toc component
    * add toc to base.scss
    * remove .scss extension from other imports in base.scss
    * create toc component
    * add component to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 // import Image from "../components/image"
 // import SEO from "../components/seo"
 import IntroSection from "../sections/intro"
+import TableOfContents from "../components/table-of-contents"
 import Section1 from "../sections/section-1"
 import Section2 from "../sections/section-2"
 import Section3 from "../sections/section-3"
@@ -13,6 +14,7 @@ import Section5 from "../sections/section-5"
 const IndexPage = () => (
   <Layout>
     <IntroSection />
+    <TableOfContents />
     <Section1 />
     <Section2 />
     <Section3 />
