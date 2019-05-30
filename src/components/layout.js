5c5cc6b3bfbf0f02b60db7eb3e3e80dc8122bbbf
zastrow <philip@zastrow.co>
2019-05-30 15:55:32 -0400

    feat: Add content from write-up DSS-61

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,22 +28,11 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Helmet>
+          <title>{data.site.siteMetadata.title}</title>
           <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/655912/7241412/css/fonts.css" />
           <link href="https://fonts.googleapis.com/css?family=Yellowtail&display=swap" rel="stylesheet" />
         </Helmet>
-        {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 1400,
-          }}
-        >
-          <main>{children}</main>
-          <footer style={{ color: "white"}}>
-            <p>© {new Date().getFullYear()} Sparkbox.</p>
-            <p>All rights reserved.</p>
-          </footer>
-        </div>
+        {children}
       </>
     )}
   />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,23 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
-import SEO from "../components/seo"
+// import SEO from "../components/seo"
+import IntroSection from "../sections/intro"
+import Section1 from "../sections/section-1"
+import Section2 from "../sections/section-2"
+import Section3 from "../sections/section-3"
+import Section4 from "../sections/section-4"
+import Section5 from "../sections/section-5"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1 class="headline-1"><span data-text="design systems">Design systems</span></h1>
-    <div className="cmp-intro">
-      <p>The 2019 Design Systems Survey is intended to illuminate how design systems are created, used, and maintained. Design Systems have the ability to solve real-life problems, establishing consistency in design and code, promoting efficiency when creating products, communicating usage guidance, encouraging reuse instead of rework, increasing the accessibility and usability of digital properties, and so much more.</p>
-      <p>For the <a href="https://designsystemssurvey.seesparkbox.com/">second year in a row</a>, <a href="https://seesparkbox.com/">Sparkbox</a>, a web design and development firm, has conducted this survey to learn more about both the challenges and benefits of building and using design systems. By analyzing the reflections of in-house teams who use a design system and agencies who’ve helped organizations build new or maintain existing design systems, we look to inform those who want to build or improve their own.</p>
-    </div>
-    <Link to="/page-2/">Go to typography and basic body copy styles on page 2</Link>
+    <IntroSection />
+    <Section1 />
+    <Section2 />
+    <Section3 />
+    <Section4 />
+    <Section5 />
   </Layout>
 )
 
