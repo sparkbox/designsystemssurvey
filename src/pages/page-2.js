3ea04491f09eb8d8ff8cae5b84a7d885ca36b7ff
Melissa Thompson <mthompson@heysparkbox.com>
2019-05-31 11:32:52 -0400

    refactor: add font prefix

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,19 +10,19 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
 
-    <h1 class="chrome">Design systems</h1>
-    <p class="accent-font">Survey</p>
-    <h2 class="chrome--small">Table of Contents</h2>
-    <p class="label-heading">The Respondents</p>
-    <h3 class="section-heading">The Respondents</h3>
-    <h4 class="primary-content-heading">The 2019 Design Systems Survey received 148 responses from in-house teams who use a design system for their organizations.</h4>
-    <h4 class="secondary-heading">Have a dedicated team to maintain the design system.</h4>
-    <p class="diagram-heading util-margin-btm0">In-House Respondents Breakdown</p>
-    <p class="diagram-copy">Respondents could select multiple from 19 options and fill out a response to “other”, 163 in-house and agency respondents</p>
-    <p class="body-copy">This survey had 256 responses comprised of two respondent types:</p>
+    <h1 class="font-chrome">Design systems</h1>
+    <p class="font-accent">Survey</p>
+    <h2 class="font-chrome font-chrome--small">Table of Contents</h2>
+    <p class="font-label-heading">The Respondents</p>
+    <h3 class="font-section-heading">The Respondents</h3>
+    <h4 class="font-primary-content-heading">The 2019 Design Systems Survey received 148 responses from in-house teams who use a design system for their organizations.</h4>
+    <h4 class="font-secondary-heading">Have a dedicated team to maintain the design system.</h4>
+    <p class="font-diagram-heading util-margin-btm0">In-House Respondents Breakdown</p>
+    <p class="font-diagram-copy">Respondents could select multiple from 19 options and fill out a response to “other”, 163 in-house and agency respondents</p>
+    <p class="font-body-copy">This survey had 256 responses comprised of two respondent types:</p>
 
-    <h2 className="label-heading">List Elements</h2>
-    <p className="body-copy">Unordered List</p>
+    <h2 className="font-label-heading">List Elements</h2>
+    <p className="font-body-copy">Unordered List</p>
     <ul>
       <li>One</li>
       <li>Two</li>
@@ -30,7 +30,7 @@ const SecondPage = () => (
       <li>Four</li>
       <li>Five</li>
     </ul>
-    <p className="body-copy">Ordered List</p>
+    <p className="font-body-copy">Ordered List</p>
     <ol>
       <li>One</li>
       <li>Two</li>
