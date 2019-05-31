8fd429ac0a75defe479d02baf7411a29ef8d7ed1
Melissa Thompson <mthompson@heysparkbox.com>
2019-05-31 14:57:26 -0400

    feat: top features component

---
+++ src/components/top-features.js
@@ -0,0 +1,51 @@
+import React from "react"
+
+ const TopFeatures = () => (
+  <section>
+    <div className="cmp-percentage-card__container">
+      <div className="cmp-percentage-card">
+        <svg className="cmp-percentage-card__icon" width="264" height="264" viewBox="0 0 264 264">
+          <g fill="none" fillRule="evenodd">
+            <path fill="#DF138C" fillOpacity="0.1" stroke="#DF138C" strokeWidth="2" d="M131.9 1C204 1 262.7 53.5 262.7 117.5a72.4 72.4 0 0 1-73 72.3h-25.1a21.5 21.5 0 0 0-21.8 21.8 22 22 0 0 0 5.4 14.3c3.4 4.1 5.5 9.5 5.5 15 0 12.3-9.6 21.8-21.8 21.8a130.9 130.9 0 0 1 0-261.7"/>
+            <path fill="#DF138C" d="M52.1 131.9c12.3 0 21.8-9.6 21.8-21.9 0-12.2-9.5-21.8-21.8-21.8A21.5 21.5 0 0 0 30.3 110c0 12.3 9.5 21.9 21.8 21.9M95.7 74c12.3 0 21.8-9.6 21.8-21.9 0-12.3-9.5-21.8-21.8-21.8A21.5 21.5 0 0 0 74 52.1C74 64.4 83.5 74 95.7 74M168 74c12.2 0 21.8-9.6 21.8-21.9 0-12.3-9.6-21.8-21.8-21.8a21.5 21.5 0 0 0-21.8 21.8c0 12.3 9.5 21.8 21.8 21.8M211.6 131.9c12.3 0 21.8-9.6 21.8-21.9 0-12.2-9.5-21.8-21.8-21.8a21.5 21.5 0 0 0-21.8 21.8c0 12.3 9.5 21.9 21.8 21.9"/>
+          </g>
+        </svg>
+        <p className="cmp-percentage-card__label font-label">Color System</p>
+        <mark className="cmp-percentage-card__percentage">92%</mark>
+      </div>
+      <div className="cmp-percentage-card">
+        <svg className="cmp-percentage-card__icon" width="194" height="250" viewBox="0 0 194 250">
+          <path fill="#15CBF4" fillOpacity="0.1" fillRule="evenodd" stroke="#15CBF4" strokeWidth="2" d="M166.2 209.1L166 96.6l-32.8 1.6h-65v-5.5c0-21 1.5-42 4.5-50.2 2-5.6 5.8-10.7 11.3-15.1a25.3 25.3 0 0 1 15.7-6.5c4.3 0 8 .7 10.7 2.2 3.8 2.2 7.6 5.9 11.2 11a82.3 82.3 0 0 0 15.5 18.6c3.6 2.6 7.9 4 12.8 4 4.8 0 9-1.8 12.5-5.4a18 18 0 0 0 5.2-13c0-5.5-2.3-11.4-6.9-17.4a46.8 46.8 0 0 0-20.3-14.5A78.2 78.2 0 0 0 111 1a81.5 81.5 0 0 0-42.4 11.2 72.6 72.6 0 0 0-28.4 30.7c-6 12-5.5 34.7-5.9 55.3H2v18.4h32.3v83.6c0 18.5-5.6 23.5-7.8 26.1-3 3.7-8.4 7.5-15.5 7.5H1v14.5h99.3v-14.5h-1.8c-23.5 0-30.1-2.5-30.1-31.1v-86.1h36c19.3 0 23.7.9 24.7 1.3 1.2.4 2.7 1.2 3.3 2.3.3.5 1.4 6.5 1.4 21.4v64.6c0 15-1.9 20-3 21.7-2.4 3.7-5.2 5.7-14 6v14.4h75.6V234c-23.6 0-26.2-2.5-26.2-24.8z"/>
+        </svg>
+        <p className="cmp-percentage-card__label font-label">Typography System</p>
+        <mark className="cmp-percentage-card__percentage">89%</mark>
+      </div>
+      <div className="cmp-percentage-card">
+        <svg className="cmp-percentage-card__icon" width="222" height="267" viewBox="0 0 222 267">
+          <path fill="#F3C448" fillOpacity="0.1" stroke="#F3C448" strokeWidth="2" d="M217 1H5a4 4 0 0 0-4 4v257a4 4 0 0 0 4 4h212a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4"/>
+          <path fill="#F3C448" fillOpacity="0.1" stroke="#F3C448" strokeWidth="2" d="M191 26a10 10 0 0 1 10 10v25a10 10 0 0 1-10 10H31a10 10 0 0 1-10-10V36a10 10 0 0 1 10-10h160zM71 91a10 10 0 0 1 10 10v40a10 10 0 0 1-10 10H31a10 10 0 0 1-10-10v-40a10 10 0 0 1 10-10h40zm0 80a10 10 0 0 1 10 10v40a10 10 0 0 1-10 10H31a10 10 0 0 1-10-10v-40a10 10 0 0 1 10-10h40z"/>
+          <path fill="#F3C448" d="M67.5 102.2a4 4 0 0 1 5.6 0l1.4 1.4a4 4 0 0 1 0 5.6L49.8 134l-1.4 1.4a4 4 0 0 1-5.7 0l-1.4-1.4-14.1-14.2a4 4 0 0 1 0-5.6l1.4-1.4a4 4 0 0 1 5.6 0L45.6 124l21.9-22zm0 82.4a4 4 0 0 1 5.6 0l1.4 1.5a4 4 0 0 1 0 5.6l-24.7 24.8-1.4 1.4a4 4 0 0 1-5.7 0l-1.4-1.4-14.1-14.2a4 4 0 0 1 0-5.6l1.4-1.5a4 4 0 0 1 5.6 0l11.4 11.4 21.9-22zm126-68.6h-85a7.5 7.5 0 0 1 0-15h85a7.5 7.5 0 0 1 0 15zm-20 25h-65a7.5 7.5 0 0 1 0-15h65a7.5 7.5 0 0 1 0 15zm20 55h-85a7.5 7.5 0 0 1 0-15h85a7.5 7.5 0 0 1 0 15zm-20 25h-65a7.5 7.5 0 0 1 0-15h65a7.5 7.5 0 0 1 0 15z"/>
+        </svg>
+        <p className="cmp-percentage-card__label font-label">Form Components</p>
+        <mark className="cmp-percentage-card__percentage">88%</mark>
+      </div>
+    </div>
+    <ul className="cmp-percentage-list">
+      <li className="cmp-percentage-list__item">Navigation components <mark className="util-bold">79%</mark></li>
+      <li className="cmp-percentage-list__item">CSS code <mark className="util-bold">70%</mark></li>
+      <li className="cmp-percentage-list__item">HTML code <mark className="util-bold">69%</mark></li>
+      <li className="cmp-percentage-list__item">Usage guidelines <mark className="util-bold">69%</mark></li>
+      <li className="cmp-percentage-list__item">Grid system <mark className="util-bold">68%</mark></li>
+      <li className="cmp-percentage-list__item">Spacing system <mark className="util-bold">66%</mark></li>
+      <li className="cmp-percentage-list__item">Layout system <mark className="util-bold">66%</mark></li>
+      <li className="cmp-percentage-list__item">Content blocks <mark className="util-bold">63%</mark></li>
+      <li className="cmp-percentage-list__item">Brand guidelines <mark className="util-bold">62%</mark></li>
+      <li className="cmp-percentage-list__item">Design files <mark className="util-bold">59%</mark></li>
+      <li className="cmp-percentage-list__item">JavaScript code <mark className="util-bold">56%</mark></li>
+      <li className="cmp-percentage-list__item">Accessibility guidelines <mark className="util-bold">55%</mark></li>
+      <li className="cmp-percentage-list__item">Framework-specific components <mark className="util-bold">53%</mark></li>
+    </ul>
+  </section>
+)
+
+ export default TopFeatures
