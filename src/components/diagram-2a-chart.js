a18529930ebf4fb86cb931f972c80d5a893a6d6f
Melissa Thompson <mthompson@heysparkbox.com>
2019-06-14 14:52:43 -0400

    refactor: chart to be responsive, hide at small screens

--- src/components/diagram-2a-chart.js
+++ src/components/diagram-2a-chart.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment, createRef } from "react"
+import React, { Component, createRef } from "react"
 import Chart2aCard from "./diagram-2a-chart-card"
 import observe from "../js/chart-animation"
 
@@ -15,10 +15,10 @@ class Chart2a extends Component {
 
   render() {
     return (
-      <Fragment>
+      <section className="cmp-diagram-chart">
         <p className="font-diagram-heading">Q: How is your design system delivered to the consumers of the design system?</p>
         <p className="font-diagram-copy">108 In-House Responses | Select one</p>
-        <dl ref={this.chart} className="cmp-diagram-chart" style={{["--height"]: 30 + "rem", ["--scale"]: 2, ["--count"]: 6}}>
+        <dl ref={this.chart} className="cmp-diagram-chart__list" style={{["--height"]: 30 + "rem", ["--scale"]: 2, ["--count"]: 6}}>
           <Chart2aCard percent="36" color="hotpink" label="In an external codebase and consumed via package manager"/>
           <Chart2aCard percent="24" color="blue" label="On a standalone documentation site where users primarily copy + paste, or download files"/>
           <Chart2aCard percent="16" color="orange" label="Included in the main codebase"/>
@@ -26,7 +26,7 @@ class Chart2a extends Component {
           <Chart2aCard percent="6" color="yellow" label="I am not sure"/>
           <Chart2aCard percent="4" color="white" label="Other"/>
         </dl>
-      </Fragment>
+      </section>
     )
   }
 }
