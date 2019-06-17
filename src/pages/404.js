f3f6141ffc8fb945e9166954535fde7cdbe6d041
Melissa Thompson <mthompson@heysparkbox.com>
2019-06-17 16:13:13 -0400

    feat: custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,20 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import ContentBlock from "../components/content-block"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <ContentBlock>
+      <div className="obj-layout-flexbox-center-vrt">
+        <section className="cmp-404">
+          <h1 className="cmp-404__title">404 Error</h1>
+          <h3 className="cmp-404__description">We can't seem to find the page you're looking for.</h3>
+          <p>Were you looking for this year's <a href="#">Design System Survey results</a>?</p>
+          <p>Or maybe <a href="#">last year's results</a>?</p>
+        </section>
+      </div>
+    </ContentBlock>
   </Layout>
 )
 
