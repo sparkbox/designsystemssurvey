b1fb69a59439d880ec1a6fd021b2786a230fd72d
Melissa Thompson <mthompson@heysparkbox.com>
2019-06-21 15:13:08 -0400

    refactor: vrt chart, include fallbacks for ie11

--- src/components/diagram-2a-chart-card.js
+++ src/components/diagram-2a-chart-card.js
@@ -1,17 +1,17 @@
-import React, { Fragment } from "react"
+import React from "react"
 
 const Chart2aCard = (props) => (
-  <Fragment>
-    <dt className="util-relative util-zindex-100 cmp-diagram-chart__container" style={{["--percent"]: props.percent + "%", ["--color"]: `var(--color-${props.color})`}}>
+  <div className="cmp-diagram-chart__item">
+    <dt className="util-relative util-zindex-100 cmp-diagram-chart__value-container" style={{"height": props.percent * 2 + "%"}}>
       <div className="cmp-diagram-chart__value">
         <div className="util-relative util-inline-block">
           {props.percent}<sup className="cmp-diagram-chart__superscript">%</sup>
         </div>
       </div>
-      <div className="cmp-diagram-chart__rectangle"></div>
+      <div className={`cmp-diagram-chart__rectangle cmp-diagram-chart__rectangle--${props.color}`}></div>
     </dt>
     <dd className="cmp-diagram-chart__label">{props.label}</dd>
-  </Fragment>
+  </div>
 )
 
 export default Chart2aCard
