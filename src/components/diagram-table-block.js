e62c71e5c77c3e21a47c775ded2db564e2028338
Melissa Thompson <mthompson@heysparkbox.com>
2019-06-27 17:24:36 -0400

    fix: console error for flexBasis

--- src/components/diagram-table-block.js
+++ src/components/diagram-table-block.js
@@ -2,7 +2,7 @@ import React, { Fragment } from "react"
 
 const TableBlock = (props) => (
   <Fragment>
-    <li className={`cmp-row-chart__block cmp-row-chart__block--${props.color}`} style={{"flexBasis": props.percent + "%", "-ms-flex-preferred-size": props.percent + "%"}}><span className="cmp-row-chart__chart-percent">{props.percent}%</span></li>
+    <li className={`cmp-row-chart__block cmp-row-chart__block--${props.color}`} style={{["flexBasis"]: props.percent + "%", "-ms-flex-preferred-size": props.percent + "%"}}><span className="cmp-row-chart__chart-percent">{props.percent}%</span></li>
   </Fragment>
 )
 
