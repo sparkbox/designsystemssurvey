5de892e48a053b8c20e2b448d17240ad15f138e3
Melissa Thompson <mthompson@heysparkbox.com>
2019-07-08 13:35:15 -0400

    feat: top tools comparison list components, styles

---
+++ src/components/top-tools-list.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+const TopToolsList = () => (
+  <section className="obj-layout-max-width cmp-feature-list__flex">
+    <div className="cmp-feature-list__container">
+      <h4 className="cmp-feature-list__title">Designer or User Experience</h4>
+      <h5 className="cmp-feature-list__description">146 Responses</h5>
+      <ol className="cmp-feature-list">
+        <li className="cmp-feature-list__item"><span>InVision Design System Manager&nbsp;<mark className="util-italic">60 responses</mark></span></li>
+        <li className="cmp-feature-list__item"><span>Storybook&nbsp;<mark className="util-italic">38 responses</mark></span></li>
+        <li className="cmp-feature-list__item"><span>Pattern Lab&nbsp;<mark className="util-italic">33 responses</mark></span></li>
+        <li className="cmp-feature-list__item"><span>UXPin&nbsp;<mark className="util-italic">31 responses</mark></span></li>
+        <li className="cmp-feature-list__item"><span>Fractal&nbsp;<mark className="util-italic">11 responses</mark></span></li>
+      </ol>
+    </div>
+
+    <div className="cmp-feature-list__container">
+      <h4 className="cmp-feature-list__title">Developer</h4>
+      <h5 className="cmp-feature-list__description">117 Responses</h5>
+      <ol className="cmp-feature-list">
+        <li className="cmp-feature-list__item"><span>Storybook&nbsp;<mark className="util-italic">47 responses</mark></span></li>
+        <li className="cmp-feature-list__item"><span>Pattern Lab&nbsp;<mark className="util-italic">26 responses</mark></span></li>
+        <li className="cmp-feature-list__item"><span>InVision Design System Manager&nbsp;<mark className="util-italic">18 responses</mark></span></li>
+        <li className="cmp-feature-list__item"><span>Fractal&nbsp;<mark className="util-italic">12 responses</mark></span></li>
+        <li className="cmp-feature-list__item"><span>UXPin&nbsp;<mark className="util-italic">11 responses</mark></span></li>
+      </ol>
+    </div>
+  </section>
+)
+
+export default TopToolsList
