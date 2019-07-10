66685c66a234c48b6ff3ff464dd1806d38d16364
zastrow <philip@zastrow.co>
2019-07-10 16:36:15 -0400

    feat: Add section header styles and component DSS-46

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,26 @@ import Layout from "../components/layout"
 // import Image from "../components/image"
 // import SEO from "../components/seo"
 import IntroSection from "../sections/intro"
-import TableOfContents from "../components/table-of-contents"
 import Section1 from "../sections/section-1"
 import Section2 from "../sections/section-2"
 import Section3 from "../sections/section-3"
 import Section4 from "../sections/section-4"
 import Section5 from "../sections/section-5"
 import Footer from "../components/footer"
+import TableOfContents from "../components/table-of-contents"
 import ContentBlock from "../components/content-block"
+import SectionHeading from "../components/section-heading"
 
 const IndexPage = () => (
   <Layout>
+    <svg width="0" height="0" className="util-visually-hidden">
+      <defs>
+        <linearGradient id="pinkyellow" x1="50%" x2="50%" y1="0%" y2="100%">
+          <stop offset="0%" stop-color="#DF138C"/>
+          <stop offset="100%" stop-color="#F3C448"/>
+        </linearGradient>
+      </defs>
+    </svg>
     <ContentBlock>
       <a href="#table-of-contents" className="cmp-skip-link">Skip to table of contents</a>
       <IntroSection />
@@ -26,7 +35,9 @@ const IndexPage = () => (
 
     <section id="section-1">
       <header>
-        <h2>The Respondents</h2>
+        <SectionHeading sectionNumber="1">
+          The Respondents
+        </ SectionHeading>
       </header>
 
       <Section1 />
@@ -34,7 +45,9 @@ const IndexPage = () => (
 
     <section id="section-2">
       <header>
-        <h2>Design System Contents and Functionality</h2>
+        <SectionHeading sectionNumber="2">
+          Design&nbsp;System Contents and Functionality
+        </ SectionHeading>
       </header>
 
       <Section2 />
@@ -42,7 +55,9 @@ const IndexPage = () => (
 
     <section id="section-3">
       <header>
-        <h2>Values and Challenges of Design Systems</h2>
+        <SectionHeading sectionNumber="3">
+          Values and Challenges of&nbsp;Design&nbsp;Systems
+        </ SectionHeading>
       </header>
 
       <Section3 />
@@ -50,7 +65,9 @@ const IndexPage = () => (
 
     <section id="section-4">
       <header>
-        <h2>Creating a Successful Design System</h2>
+        <SectionHeading sectionNumber="4">
+          Creating a Successful Design&nbsp;System
+        </ SectionHeading>
       </header>
 
       <Section4 />
@@ -58,7 +75,9 @@ const IndexPage = () => (
 
     <section id="section-5">
       <header>
-        <h2>Learn More about Design Systems</h2>
+        <SectionHeading sectionNumber="5">
+          Learn More about Design&nbsp;Systems
+        </ SectionHeading>
       </header>
 
       <Section5 />
