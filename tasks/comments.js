672740084d47837fc090f3e42c359db868aa4e14
Melissa Thompson <mthompson@heysparkbox.com>
2019-07-11 10:49:23 -0400

    feat: add conditional comments to 2019 page

--- tasks/comments.js
+++ tasks/comments.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const regex = RegExp('<html>');
 const comment = '<!--[if lt IE 7]><html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7" lang="en"><![endif]--><!--[if IE 7]><html class="no-js lt-ie10 lt-ie9 lt-ie8" lang="en"><![endif]--><!--[if IE 8]><html class="no-js lt-ie10 lt-ie9" lang="en"><![endif]--><!--[if IE 9]><html class="no-js lt-ie10" lang="en"><![endif]--><!--[if gt IE 9]><!--> <html class="no-js" lang="en"> <!--<![endif]-->';
 
-const addCommentsToIndex = (newFile, originalFile) => {
+const addCommentsToPage = (newFile, originalFile) => {
   fs.readFile(originalFile, 'utf8', (err, data) => {
     if (err) throw err;
     const htmlSearch = regex.exec(data);
@@ -17,4 +17,5 @@ const addCommentsToIndex = (newFile, originalFile) => {
   });
 }
 
-addCommentsToIndex('./public/index.html', './public/index.html');
+addCommentsToPage('./public/index.html', './public/index.html');
+addCommentsToPage('./public/2019/index.html', './public/2019/index.html');
