ba6ecc1483ebfacb8db68e0e3562747b7fdaacab
Melissa Thompson <mthompson@heysparkbox.com>
2019-07-11 16:20:56 -0400

    refactor: section header to be responsive

--- src/components/section-header.js
+++ src/components/section-header.js
@@ -4,8 +4,8 @@ import PropTypes from "prop-types"
 const SectionHeader = ({ sectionNumber, children }) => (
   <header className={"cmp-section-header cmp-section-header--" + sectionNumber}>
     <div className="cmp-section-header__number-box" aria-label={"Section " + sectionNumber}>
-      <svg width="800" height="900" viewBox="0 0 800 900" class="cmp-section-header__svg">
-        <text className="cmp-section-header__number" stroke="url(#pinkyellow)" font-size="1140">
+      <svg viewBox="0 0 800 900" className="cmp-section-header__svg">
+        <text className="cmp-section-header__number" stroke="url(#pinkyellow)" fontSize="1140">
           <tspan x="400" y="850">{sectionNumber}</tspan>
         </text>
       </svg>
