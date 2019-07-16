a108e17a2176da0b135efcb3f2a921643b54119d
Heather Taylor <heather@heysparkbox.com>
2019-07-16 14:46:06 -0400

    feat: update headings for proper semantics and improved accessibility
    https://sparkbox.atlassian.net/browse/DSS-104

--- src/components/top-tools-list.js
+++ src/components/top-tools-list.js
@@ -3,8 +3,8 @@ import React from "react"
 const TopToolsList = () => (
   <section className="obj-layout-max-width cmp-feature-list__flex">
     <div className="cmp-feature-list__container">
-      <h4 className="cmp-feature-list__title">Designer or User Experience</h4>
-      <h5 className="cmp-feature-list__description">146 Responses</h5>
+      <h3 className="cmp-feature-list__title">Designer or User Experience</h3>
+      <h4 className="cmp-feature-list__description">146 Responses</h4>
       <ol className="cmp-feature-list">
         <li className="cmp-feature-list__item"><span>InVision Design System Manager&nbsp;<mark className="util-italic">60 responses</mark></span></li>
         <li className="cmp-feature-list__item"><span>Storybook&nbsp;<mark className="util-italic">38 responses</mark></span></li>
@@ -15,8 +15,8 @@ const TopToolsList = () => (
     </div>
 
     <div className="cmp-feature-list__container">
-      <h4 className="cmp-feature-list__title">Developer</h4>
-      <h5 className="cmp-feature-list__description">117 Responses</h5>
+      <h3 className="cmp-feature-list__title">Developer</h3>
+      <h4 className="cmp-feature-list__description">117 Responses</h4>
       <ol className="cmp-feature-list">
         <li className="cmp-feature-list__item"><span>Storybook&nbsp;<mark className="util-italic">47 responses</mark></span></li>
         <li className="cmp-feature-list__item"><span>Pattern Lab&nbsp;<mark className="util-italic">26 responses</mark></span></li>
