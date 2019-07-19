30b1eff2f28a8433db366872bb7045f4c933744a
zastrow <philip@zastrow.co>
2019-07-19 15:15:44 -0400

    fix: Address semantics for Safari Reader Mode DSS-111

--- src/pages/2019.js
+++ src/pages/2019.js
@@ -34,57 +34,54 @@ const Page2019 = () => (
         </linearGradient>
       </defs>
     </svg>
-    <header>
-      <Hero />
-    </header>
-    <main>
-      <section>
-        <ContentBlock>
-          <a href="#table-of-contents" className="cmp-skip-link">Skip to table of contents</a>
-          <IntroSection />
-        </ContentBlock>
+    <article>
+      <header>
+        <Hero />
+      </header>
+      <a href="#table-of-contents" className="cmp-skip-link">Skip to table of contents</a>
+      <ContentBlock>
+        <IntroSection />
+      </ContentBlock>
 
-        <div>
-          <TableOfContents />
-        </div>
+      <div>
+        <TableOfContents />
+      </div>
 
-        <article id="section-1" className="util-margin-vrt-4">
-            <SectionHeader sectionNumber="1">
-              The Respondents
-            </ SectionHeader>
-          <Section1 />
-        </article>
-
-        <article id="section-2" className="util-margin-vrt-4">
-            <SectionHeader sectionNumber="2">
-              Design System Contents and Functionality
-            </ SectionHeader>
-          <Section2 />
-        </article>
-
-        <article id="section-3" className="util-margin-vrt-4">
-            <SectionHeader sectionNumber="3">
-              Values and Challenges of Design Systems
-            </ SectionHeader>
-          <Section3 />
-        </article>
-
-        <article id="section-4" className="util-margin-vrt-4">
-            <SectionHeader sectionNumber="4">
-              Creating a Successful Design System
-            </ SectionHeader>
-          <Section4 />
-        </article>
-
-        <article id="section-5" className="util-margin-vrt-4">
-            <SectionHeader sectionNumber="5">
-              Learn More about Design Systems
-            </ SectionHeader>
-          <Section5 />
-        </article>
+      <section id="section-1" className="util-margin-vrt-4">
+        <SectionHeader sectionNumber="1">
+          The Respondents
+        </ SectionHeader>
+        <Section1 />
       </section>
-    </main>
 
+      <section id="section-2" className="util-margin-vrt-4">
+        <SectionHeader sectionNumber="2">
+          Design System Contents and Functionality
+        </ SectionHeader>
+        <Section2 />
+      </section>
+
+      <section id="section-3" className="util-margin-vrt-4">
+        <SectionHeader sectionNumber="3">
+          Values and Challenges of Design Systems
+        </ SectionHeader>
+        <Section3 />
+      </section>
+
+      <section id="section-4" className="util-margin-vrt-4">
+        <SectionHeader sectionNumber="4">
+          Creating a Successful Design System
+        </ SectionHeader>
+        <Section4 />
+      </section>
+
+      <section id="section-5" className="util-margin-vrt-4">
+        <SectionHeader sectionNumber="5">
+          Learn More about Design Systems
+        </ SectionHeader>
+        <Section5 />
+      </section>
+    </article>
     <Footer />
   </Layout>
 )
