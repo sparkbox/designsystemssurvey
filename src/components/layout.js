3cead2d08baa91d13bff38be98676cf479901914
Melissa Thompson <melissa_a_thompson@gap.com>
2020-02-20 13:33:02 -0500

    fix: put meta description on page
    * move all meta data into layout.js
    * remove all meta data from 2019.js page, this should only contain page
    specific code

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,6 +15,7 @@ import "../js/chart-animation"
 import "../js/safe-focus"
 import "../scss/base.scss"
 
+// siteMetadata comes from gatsby-config.js
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
@@ -22,6 +23,9 @@ const Layout = ({ children }) => (
         site {
           siteMetadata {
             title
+            name
+            author
+            description
           }
         }
       }
@@ -30,6 +34,18 @@ const Layout = ({ children }) => (
       <>
         <Helmet>
           <title>{data.site.siteMetadata.title}</title>
+          <meta name="name" content={data.site.siteMetadata.name} />
+          <meta name="author" content={data.site.siteMetadata.author} />
+          <meta name="description" content={data.site.siteMetadata.description} />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="/images/ogimage-2019.jpg" />
+          <meta property="og:locale" content="en_US" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@hearsparkbox" />
+
+
           <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/655912/7241412/css/fonts.css" />
 
           {/* This will change based on the current year */}
