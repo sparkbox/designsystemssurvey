bc40536a97587035ef048cf6c9fbe5dcd1efb804
emilykggray <emily@heysparkbox.com>
2020-09-03 14:37:04 -0400

    remove last year reference

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,7 +10,6 @@ const NotFoundPage = () => (
         <h1 className="cmp-404__title">404 Error</h1>
         <h3 className="cmp-404__description">We can't seem to find the page you're looking for.</h3>
         <p>Were you looking for this year's <a href="https://designsystemssurvey.seesparkbox.com/">Design System Survey results</a>?</p>
-        <p>Or maybe <a href="https://designsystemssurvey.seesparkbox.com/2018">last year's results</a>?</p>
       </section>
     </ContentBlock>
   </Layout>
