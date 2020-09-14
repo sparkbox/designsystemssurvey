89678e57e20c619683b596c6eaa4aba1d4205a69
zastrow <philip@zastrow.co>
2020-09-14 16:29:01 -0400

    feat: content updates

---
+++ src/components/2020/newsletter-form.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const NewsletterForm = () => (
+  <div id="mc_embed_signup">
+    <form action="https://seesparkbox.us1.list-manage.com/subscribe/post?u=c2fcafb7ccc2db34e99075bb1&amp;id=2835f91fa5" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate util-text-center" target="_blank" novalidate>
+      <div id="mc_embed_signup_scroll">
+        <label class="util-visually-hidden" for="mce-EMAIL">Email Address</label>
+        <input type="email" value="" name="EMAIL" class="cmp-input email" id="mce-EMAIL" placeholder="you@example.com" required />
+        <div class="util-visually-hidden" aria-hidden="true">
+          <input type="text" name="b_c2fcafb7ccc2db34e99075bb1_2835f91fa5" tabindex="-1" value="" />
+        </div>
+        <button class="cmp-button" type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe">
+          Subscribe
+        </button> 
+      </div>
+    </form>
+  </div>
+)
+
+export default NewsletterForm
