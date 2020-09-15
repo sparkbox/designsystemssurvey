4d2a9970d35bab038bf9b791958677a648400d95
Adam Simpson <adam@heysparkbox.com>
2020-09-15 11:34:47 -0400

    chore: remove console.log

--- src/js/2020/intersect.js
+++ src/js/2020/intersect.js
@@ -1,29 +1,30 @@
-require('intersection-observer');
+require('intersection-observer')
 
 export default function observe(platform, animateClass) {
-
   if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.intersectionRatio > 0) {
+            const animationElement = entry.target
+            const animationType = animationElement.dataset.animate
 
-    const observer = new IntersectionObserver((entries, observer) => { 
-      entries.forEach(entry => {  
-        if (entry.intersectionRatio > 0) {
-          const animationElement = entry.target;
-          const animationType = animationElement.dataset.animate;
+            if (!animationType) {
+              return
+            }
 
-          if (!animationType) { return; }
-    
-          animationElement.classList.add(animationType);
-          observer.unobserve(entry.target);
-        }
-      });
-    }, {
-      rootMargin: "0px 0px -200px 0px"
-    });
+            animationElement.classList.add(animationType)
+            observer.unobserve(entry.target)
+          }
+        })
+      },
+      {
+        rootMargin: '0px 0px -200px 0px',
+      }
+    )
 
-    document.querySelectorAll('.js-watch').forEach(el => {
-      console.log(el);
+    document.querySelectorAll('.js-watch').forEach((el) => {
       observer.observe(el)
-    });
-
+    })
   }
 }
