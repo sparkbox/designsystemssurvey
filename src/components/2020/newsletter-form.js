bd1dccd80908316d722f7eabc08bdbe13fecc7be
zastrow <philip@zastrow.co>
2020-09-15 12:48:32 -0400

    fix: Add mailchimp signup location

--- src/components/2020/newsletter-form.js
+++ src/components/2020/newsletter-form.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const NewsletterForm = () => (
   <div id="mc_embed_signup">
-    <form action="https://seesparkbox.us1.list-manage.com/subscribe/post?u=c2fcafb7ccc2db34e99075bb1&amp;id=2835f91fa5" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate util-text-center" target="_blank" novalidate>
+    <form action="https://seesparkbox.us1.list-manage.com/subscribe/post?u=c2fcafb7ccc2db34e99075bb1&amp;id=2835f91fa5&SIGNUP=2020DesignSurvey" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate util-text-center" target="_blank" novalidate>
       <div id="mc_embed_signup_scroll">
         <label className="util-visually-hidden" for="mce-EMAIL">Email Address</label>
         <input type="email" name="EMAIL" class="cmp-input email" id="mce-EMAIL" placeholder="you@example.com" required />
