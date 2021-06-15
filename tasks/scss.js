5296309ed500465514bfb07366af02fb1a476747
zastrow <philip@zastrow.co>
2021-06-15 17:10:40 -0400

    feat: Add BEM structure and font styles

--- tasks/scss.js
+++ tasks/scss.js
@@ -5,6 +5,7 @@ const autoprefixer = require('autoprefixer')
 const postcss = require('postcss')
 
 const years = ['2019', '2020', '2021']
+const sparkle = path.resolve('node_modules/@sparkbox')
 
 years.forEach((year) => {
   const base = path.resolve(`src/scss/${year}/base.scss`)
@@ -12,6 +13,7 @@ years.forEach((year) => {
   sass.render(
     {
       file: base,
+      includePaths: [sparkle]
     },
     (err, result) => {
       const location = path.resolve(`./static/css/${year}.css`)
