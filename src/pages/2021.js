cfc7f284d3b13d72e69e59b68f3c4874b75ca23e
zastrow <philip@zastrow.co>
2021-06-17 09:49:18 -0400

    feat: vertical lines background

--- src/pages/2021.js
+++ src/pages/2021.js
@@ -12,16 +12,18 @@ class Page2021 extends Component {
           image="ogimage-2021.png"
           year="2021"
         />
-        <main>
-          <h1 className="cmp-type-hero">Design Systems Survey</h1>
-          <h2 className="cmp-type-section">Section Title</h2>
-          <h2>Heading Level Two</h2>
-          <h3>Heading Level Three</h3>
-          <p clasName="cmp-type-large">Large Body Type. Lorem ipsum dolor sit amet consectetur adipiscing elit porttitor nam, maecenas nullam posuere ornare leo lacinia fermentum tempor habitasse donec, eget aliquet volutpat felis hac nec proin torquent. Ornare varius primis neque id fringilla felis penatibus nullam eget, facilisis aliquam condimentum sociosqu volutpat molestie maecenas. Ligula condimentum nam ac lacinia sociosqu ornare elementum odio, nec mollis luctus parturient enim senectus quam, et vitae orci torquent litora dui turpis. Eros praesent euismod sed etiam non bibendum imperdiet nulla, conubia risus proin lobortis faucibus ut phasellus suspendisse ornare, quam at lectus taciti nostra fusce vehicula.</p>
-          <p>Default Body Type. Lorem ipsum dolor sit amet consectetur adipiscing elit curae dapibus, maecenas pretium quis eu leo magnis nunc consequat, tempor etiam mollis penatibus feugiat volutpat arcu porttitor. Class venenatis purus orci volutpat facilisi primis vitae leo, dui sociosqu pharetra dictumst ipsum molestie taciti, nec mollis condimentum nascetur ante turpis felis. Accumsan in cubilia ornare pulvinar imperdiet litora, neque luctus cras laoreet convallis non arcu, sagittis sapien conubia lobortis commodo. Convallis rutrum accumsan eu varius taciti porta in, per donec metus facilisi libero dictumst dignissim, posuere dapibus auctor nostra mauris augue. Habitasse ornare at est placerat mattis phasellus proin, id felis justo taciti hendrerit ac malesuada tortor, nascetur turpis dignissim suspendisse consectetur hac. A gravida congue mus integer suscipit, ullamcorper sociis porta ridiculus.</p>
-          <p className="cmp-type-sans-micro">Lorem ipsum dolor sit amet</p>
-          <p className="cmp-type-mono-micro">Feugiat volutpat arcu porttitor</p>
-        </main>
+        <div className="obj-lines obj-max-width">
+          <main>
+            <h1 className="cmp-type-hero">Design Systems Survey</h1>
+            <h2 className="cmp-type-section">Section Title</h2>
+            <h2>Heading Level Two</h2>
+            <h3>Heading Level Three</h3>
+            <p clasName="cmp-type-large">Large Body Type. Lorem ipsum dolor sit amet consectetur adipiscing elit porttitor nam, maecenas nullam posuere ornare leo lacinia fermentum tempor habitasse donec, eget aliquet volutpat felis hac nec proin torquent. Ornare varius primis neque id fringilla felis penatibus nullam eget, facilisis aliquam condimentum sociosqu volutpat molestie maecenas. Ligula condimentum nam ac lacinia sociosqu ornare elementum odio, nec mollis luctus parturient enim senectus quam, et vitae orci torquent litora dui turpis. Eros praesent euismod sed etiam non bibendum imperdiet nulla, conubia risus proin lobortis faucibus ut phasellus suspendisse ornare, quam at lectus taciti nostra fusce vehicula.</p>
+            <p>Default Body Type. Lorem ipsum dolor sit amet consectetur adipiscing elit curae dapibus, maecenas pretium quis eu leo magnis nunc consequat, tempor etiam mollis penatibus feugiat volutpat arcu porttitor. Class venenatis purus orci volutpat facilisi primis vitae leo, dui sociosqu pharetra dictumst ipsum molestie taciti, nec mollis condimentum nascetur ante turpis felis. Accumsan in cubilia ornare pulvinar imperdiet litora, neque luctus cras laoreet convallis non arcu, sagittis sapien conubia lobortis commodo. Convallis rutrum accumsan eu varius taciti porta in, per donec metus facilisi libero dictumst dignissim, posuere dapibus auctor nostra mauris augue. Habitasse ornare at est placerat mattis phasellus proin, id felis justo taciti hendrerit ac malesuada tortor, nascetur turpis dignissim suspendisse consectetur hac. A gravida congue mus integer suscipit, ullamcorper sociis porta ridiculus.</p>
+            <p className="cmp-type-sans-micro">Lorem ipsum dolor sit amet</p>
+            <p className="cmp-type-mono-micro">Feugiat volutpat arcu porttitor</p>
+          </main>
+        </div>
       </Layout>
     )
   }
