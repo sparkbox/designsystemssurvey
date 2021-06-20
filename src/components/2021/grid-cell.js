e7c3ce56e9a0ac59c813c607a523996540aeb3a9
zastrow <philip@zastrow.co>
2021-06-20 14:45:32 -0400

    fix: Update grid system to allow for row placement

--- src/components/2021/grid-cell.js
+++ src/components/2021/grid-cell.js
@@ -7,14 +7,17 @@ const GridCell = (props) => {
   cssProperties['--span'] = props.span
   cssProperties['--align'] = props.align
   cssProperties['--row-span'] = props.rowSpan
+  cssProperties['--row-start'] = props.rowStart
   cssProperties['--start-md'] = props.startMD
   cssProperties['--span-md'] = props.spanMD
   cssProperties['--align-md'] = props.alignMD
   cssProperties['--row-span-md'] = props.rowSpanMD
+  cssProperties['--row-start-md'] = props.rowStartMD
   cssProperties['--start-lg'] = props.startLG
   cssProperties['--span-lg'] = props.spanLG
   cssProperties['--align-lg'] = props.alignLG
   cssProperties['--row-span-lg'] = props.rowSpanLG
+  cssProperties['--row-start-lg'] = props.rowStartLG
 
   return (
     <div className="obj-grid__col" style={cssProperties}>
@@ -28,14 +31,17 @@ GridCell.defaultProps = {
   span: 4,
   align: 'top',
   rowSpan: 1,
+  rowStart: 'auto',
   startMD: 'var(--start)',
   spanMD: 'var(--span)',
   alignMD: 'var(--align)',
   rowSpanMD: 'var(--row-span)',
+  rowStartMD: 'var(--row-start)',
   startLG: 'var(--start-md)',
   spanLG: 'var(--span-md)',
   alignLG: 'var(--align-md)',
-  rowSpanLG: 'var(--row-span-md)'
+  rowSpanLG: 'var(--row-span-md)',
+  rowStartLG: 'var(--row-start-md)'
 }
 
 GridCell.propTypes = {
