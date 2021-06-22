d8f8d53e350a5b000a566154925c7858abbddd08
zastrow <philip@zastrow.co>
2021-06-22 12:16:39 -0400

    feat: Blockquotes

---
+++ src/components/2021/quote.js
@@ -0,0 +1,25 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const Quote = (props) => {
+  let quoteType = (props.style === 'small') ?  'cmp-type-body-large' : 'cmp-type-h2';
+
+  return (
+    <figure className={`cmp-quote cmp-quote--${props.style}`}>
+      <blockquote className="cmp-quote__block">
+        <p className={`${quoteType} cmp-quote__text`}>
+          {props.quote}<span aria-hidden="true">&rdquo;</span>
+        </p>
+      </blockquote>
+      <figcaption className="cmp-quote__footer">
+        <div className="cmp-type-mono-micro cmp-quote__cite">{props.cite}</div>
+      </figcaption>
+    </figure>
+  )
+}
+
+Quote.defaultProps = {
+  style: 'small'
+}
+
+export default Quote
