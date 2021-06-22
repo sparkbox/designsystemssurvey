bfceba0d0362b85467b0c7edb452b8f03e7d9e36
zastrow <philip@zastrow.co>
2021-06-22 08:46:02 -0400

    feat: Add Observation Heading

---
+++ src/components/2021/observation-heading.js
@@ -0,0 +1,32 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const ObservationHeading = (props) => {
+  let sizeClasses = []
+
+  if (props.size != null) {
+    sizeClasses.push(` cmp-observation--${props.size}`);
+  }
+  if (props.sizeMD != null) {
+    sizeClasses.push(` cmp-observation--${props.sizeMD}@md`)
+  }
+  if (props.sizeLG != null) {
+    sizeClasses.push(` cmp-observation--${props.sizeLG}@lg`)
+  }
+
+  return (
+    <div className={`cmp-observation${sizeClasses.join(" ")}`}>
+      <div className="cmp-observation__content">
+        Observation
+      </div>
+    </div>
+  )
+}
+
+ObservationHeading.defaultProps = {
+  size: 'half',
+  sizeMD: null,
+  sizeLG: null
+}
+
+export default ObservationHeading
