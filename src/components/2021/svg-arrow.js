1ce5caa7073355241623d810e967cda6b8c6ff41
aspencer116 <andrew@heysparkbox.com>
2021-06-23 16:46:08 -0400

    fix: Styling updates
    - Layout CSS adjustments
    - Make the left and right columns wider and give them a variable
    - Increase the max width of the page
    - Slightly thicker arrow icon
    - Adjust font weight and line height of large paragraphs

--- src/components/2021/svg-arrow.js
+++ src/components/2021/svg-arrow.js
@@ -3,7 +3,7 @@ import React from "react"
 const SVGArrow = (props) => {
   return (
     <svg viewBox="0 0 28 28" {...props} fill="none">
-      <path stroke-width="3" d="M2,26 26,2M2,6 2,26 22,26" />
+      <path stroke-width="4" d="M2,26 26,2M2,6 2,26 22,26" />
     </svg>
   )
 }
