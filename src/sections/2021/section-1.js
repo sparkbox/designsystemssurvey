f6e36dede8d37020155ff150ecff68ab8f11e195
aspencer116 <andrew@heysparkbox.com>
2021-06-25 16:58:05 -0400

    feat: Add spacing and layout properties to section 1

--- src/sections/2021/section-1.js
+++ src/sections/2021/section-1.js
@@ -6,31 +6,31 @@ import BarChart from '../../components/2021/bar-chart'
 import Figure from "../../components/2021/figure"
 
 const Section1 = () => (
-  <Grid>
+  <Grid className="util-margin-bottom-20vh">
     <GridCell spanMD="8" spanLG="12">
       <SectionHeader title="The Respondents" number="1" total="6" />
     </GridCell>
-  
-    <GridCell spanLG="6">
+
+    <GridCell spanLG="6" className="util-margin-bottom-1xl">
       <p class="cmp-type-body-large">In-house and agency respondents were asked a different set of questions with overlapping topics. In-house respondents offered insight based on their direct perspectives with design systems at their organizations while agency respondents offered insight into their client’s experiences.</p>
     </GridCell>
-    
-    <GridCell>
+
+    <GridCell spanLG="4" className="util-margin-bottom-1xl">
       <Figure count="1.1" caption="376 Responses">
         <p>376 Responses</p>
         <p>217 <em>In-House Respondents with design systems at their organization</em></p>
         <p>159 Agency Respondents who have worked on a design system within the last year</p>
       </Figure>
     </GridCell>
-  
-    <GridCell>
+
+    <GridCell start="2" startMD="6" startLG="9" className="util-margin-bottom-1xl util-margin-top-20vh@md">
       <Figure count="1.2" caption="Responses: In-house: 217; Agency: 159">
         <h2 class="cmp-type-h3">What is your primary discipline?</h2>
         <h3><em>In-House</em></h3>
         <p>39% <em>Development</em></p>
         <p>36% <em>Design</em></p>
         <p>12% <em>User Experience</em></p>
-        
+
         <h3>Agency</h3>
         <p>52% Development</p>
         <p>21% Design</p>
@@ -39,7 +39,7 @@ const Section1 = () => (
       <p>The remainder of the respondents were split between project/product management, management, and other.</p>
     </GridCell>
 
-    <GridCell>
+    <GridCell spanLG="6" className="util-margin-bottom-1xl util-margin-bottom-1xl@md">
       <Figure count="1.3" caption="Responses: In-house: 217; Agency: 159">
         <BarChart
           headingLevel="h3"
@@ -57,8 +57,8 @@ const Section1 = () => (
         />
       </Figure>
     </GridCell>
-    
-    <GridCell>
+
+    <GridCell start="2" startMD="6" startLG="9" className="util-margin-bottom-1xl util-margin-top-20vh@md">
       <Figure count="1.4" caption="Responses: In-house: 217; Agency: 157">
         <BarChart
           headingLevel="h3"
@@ -75,8 +75,8 @@ const Section1 = () => (
         </BarChart>
       </Figure>
     </GridCell>
-  
-    <GridCell>
+
+    <GridCell span="3" spanMD="6" spanLG="4">
       <h3><em>What is your organization’s industry?</em></h3>
       <p>In-house respondents represented a diverse array of industries such as finance/financial services, telecommunications/IT hardware & software, healthcare & pharmaceuticals, and many more.</p>
     </GridCell>
