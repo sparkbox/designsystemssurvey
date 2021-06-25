61b02ef4416fc93b3ad7ed7f0c6226c18dc8e9f5
aspencer116 <andrew@heysparkbox.com>
2021-06-25 10:54:05 -0400

    fix: Various copy updates for section 2
    - Updates the charts to use the new format

--- src/sections/2021/section-3.js
+++ src/sections/2021/section-3.js
@@ -15,17 +15,17 @@ const Section3 = () => (
     <GridCell spanMD="8" spanLG="12">
       <SectionHeader title="Priorities and Challenges" number="3" total="6" />
     </GridCell>
-    
+
     <GridCell>
       <hr />
-      <p>As noted in 2020, “Due to the nature of a design system requiring buy-in and support from multiple disciplines, a design system project often helps break down organizational silos.” Design system teams focus on common areas—many of which highlight the importance of cross-team support.</p>
+      <p className="cmp-type-body-large"><a href="/2020/">As noted in 2020</a>, “Due to the nature of a design system requiring buy-in and support from multiple disciplines, a design system project often helps break down organizational silos.” Design system teams focus on common areas—many of which highlight the importance of cross-team support.</p>
     </GridCell>
 
     <GridCell>
       <Figure count="3.1" caption="Responses: 154 | Respondents were asked to select all that apply.">
         <BarChart
           headingLevel="h3"
-          title={<>What are the top <em>priorities</em> your design system team has at this moment?</>}
+          title="Priorities"
           keyMap={['']}
           dataPoints={[
             ['Adoption (developer, designer, etc.)', [42]],
@@ -41,8 +41,10 @@ const Section3 = () => (
             ['Finding an executive champion', [7]],
             ['Subscriber engagement', [4]],
             ['Other (please specify)', [6]]
-          ]}
-        />
+          ]
+        }>
+          <p>"What are the <em>top priorities</em> your design system team has at this moment?"</p>
+        </BarChart>
       </Figure>
     </GridCell>
 
@@ -50,7 +52,7 @@ const Section3 = () => (
     <Figure count="3.2" caption="Responses: 158 | Respondents were asked to select all that apply.">
       <BarChart
         headingLevel="h3"
-        title={<>What are the top <em>challenges</em> your design system team is facing at this moment</>}
+        title="Challenges"
         keyMap={['']}
         dataPoints={[
           ['Overcoming technical/creative debt', [47]],
@@ -66,8 +68,10 @@ const Section3 = () => (
           ['Finding an executive champion', [16]],
           ['Subscriber engagement', [6]],
           ['Other (please specify)', [7]]
-        ]}
-      />
+        ]
+      }>
+        <p>"What are the <em>top challenges</em> your design system team is facing at this moment"</p>
+      </BarChart>
     </Figure>
     </GridCell>
 
@@ -100,20 +104,23 @@ const Section3 = () => (
 
     <GridCell>
       <h2 class="cmp-type-h3">Adoption continues to be a focus for many teams</h2>
-      <p>Adoption difficulties have been high on respondents’ lists of concerns since our 2018 Design Systems Survey and remain there in 2021.</p>
+      <p>Adoption difficulties have been high on respondents’ lists of concerns since our <a href="/2018/">2018 Design Systems Survey</a> and remain there in 2021.</p>
     </GridCell>
 
     <GridCell>
-      <Quote styleFormat="small"
-      cite="-Individual contributor, moderately successful design system">
-        <p>When used well it has had a large uplift in sales and performance. However, teams still need to be pushed to use it properly.</p>
+      <Quote
+        styleFormat="small"
+        cite="Individual contributor, moderately successful design system">
+        <p>When used well it has had a large uplift in sales and performance. However, teams <em><strong>still need to be pushed</strong></em> to use it properly.</p>
       </Quote>
     </GridCell>
 
     <GridCell>
-      <Quote styleFormat="small"
-      cite="-Individual contributor, unsuccessful design system">
-        <p>Too few people (developers and designers) use it on a daily basis.</p>
+      <Quote
+        styleFormat="small"
+        decoStyle="alt"
+        cite="Individual contributor, unsuccessful design system">
+        <p><em><strong>Too few people</strong></em> (developers and designers) use it on a daily basis.</p>
       </Quote>
     </GridCell>
 
@@ -121,7 +128,7 @@ const Section3 = () => (
       <Figure count="3.5" caption="Responses: 157 | Respondents were asked to select all that apply for “What are the top challenges your design system team is facing at this moment?">
         <BarChart
           headingLevel="h3"
-          title={<>Adoption is linked to success</>}
+          title="Adoption is linked to success"
           keyMap={['']}
           dataPoints={[
             ['Very Successful', [9]],
@@ -131,7 +138,7 @@ const Section3 = () => (
             ['Not Successful', [83]]
           ]}
         >
-        <p>In your opinion, how successful is your organization’s design system?" based on answering "adoption" for "what are the top challenges your design system team is facing at this moment?</p>
+        <p>"In your opinion, how <em>successful</em> is your organization’s design system?" <strong>based on answering "adoption" for</strong> "what are the <em>top challenges</em> your design system team is facing at this moment?"</p>
         </BarChart>
       </Figure>
     </GridCell>
@@ -144,7 +151,7 @@ const Section3 = () => (
       <Figure count="3.6" caption="Responses: 159">
         <StackedChart
           headingLevel="h3"
-          title={<>Higher use of the design system is linked to success</>}
+          title="Higher use of the design system is linked to success"
           keyMap={[
             'Less than 25% is from the design system',
             '25% to 49% is from the design system',
@@ -160,7 +167,7 @@ const Section3 = () => (
             ['Very Successful', [8, 8, 25, 50, 8]]
           ]}
         >
-        <p>In your opinion, <em>how successful</em> is your organization’s design system?” based on “how much of your website(s) or application(s) is sourced from your design system?</p>
+        <p>"In your opinion, how <em>successful</em> is your organization’s design system?” <strong>based on</strong> “how much of your website(s) or application(s) is <em>sourced</em> from your design system?"</p>
         </StackedChart>
       </Figure>
     </GridCell>
@@ -173,12 +180,16 @@ const Section3 = () => (
 
     <GridCell>
       <CallToAction mark="right">
+      <svg style={{maxWidth: 4 + 'rem'}} viewBox="0 0 64 64">
+        <path style={{fill: 'var(--color-neutral-1)'}} d="M62 1c.6 0 1 .4 1 1v26.5L35.5 1H62m0-1H33.1L64 30.9V2c0-1.1-.9-2-2-2zM63 35.5V62c0 .6-.4 1-1 1H35.5L63 35.5m1-2.4L33.1 64H62c1.1 0 2-.9 2-2V33.1zM1 35.5L28.5 63H2c-.6 0-1-.4-1-1V35.5m-1-2.4V62c0 1.1.9 2 2 2h28.9L0 33.1zM28.5 1L1 28.5V2c0-.6.4-1 1-1h26.5m2.4-1H2C.9 0 0 .9 0 2v28.9L30.9 0zM26 24.9h12c.6 0 1.1.5 1.1 1.1v12c0 .6-.5 1.1-1.1 1.1H26c-.6 0-1.1-.5-1.1-1.1V26c0-.6.5-1.1 1.1-1.1z M30 14.8H14.9V30zM14.9 34v15.2H30zM49.1 30V14.8H34zM34 49.2h15.1V34z"/>
+      </svg>
+
       <h3>Reaching adoption goals is a common sign of maturity for design system teams. </h3>
 
       <p>Has your design system met its adoption goals?</p>
       <p>Answer a few questions to find out where you fall within the Design System Maturity Model and get insights and ideas about how you can take your system to the next level. Take Assessment</p>
-        
-      <a href="https://example.com" className="cmp-button cmp-button--inverse">Take the Survey (Default)</a>
+
+      <a href="https://example.com" className="cmp-button cmp-button--inverse">Take the Survey</a>
       </CallToAction>
     </GridCell>
 
@@ -218,7 +229,7 @@ const Section3 = () => (
             ['Very Successful', [50, 20, 30]]
           ]}
         >
-      <p>In your opinion, <em>how successful</em> is your organization’s design system?” based on “how frequently do design system users contribute to the design system?</p>
+      <p>"In your opinion, how <em>successful</em> is your organization’s design system?” <strong>based on</strong> “how <em>frequently</em> do design system users contribute to the design system?"</p>
       </StackedChart>
       </Figure>
     </GridCell>
@@ -233,7 +244,7 @@ const Section3 = () => (
       <Figure count="3.9" caption="Responses: 136 | Answers were on a scale of 1 to 5.">
         <BarChart
           headingLevel="h3"
-          title={<>But contribution is low</>}
+          title="But contribution is low"
           keyMap={['']}
           dataPoints={[
             ['They don’t or rarely contribute to the design system', [17]],
@@ -243,7 +254,7 @@ const Section3 = () => (
             ['They always contribute to the design system', [8]],
           ]}
         >
-        <p>How frequently do design system users contribute to the design system?</p>
+        <p>"How <em>frequently</em> do design system users contribute to the design system?"</p>
       </BarChart>
       </Figure>
     </GridCell>
@@ -258,7 +269,7 @@ const Section3 = () => (
       <Figure count="3.10" caption="Respondents: 136 | Answers were on a scale of 1 to 5.">
         <StackedChart
           headingLevel="h3"
-          title={<>More defined process is linked to increased contribution</>}
+          title="More defined process is linked to increased contribution"
           keyMap={[
             'They don’t or rarely contribute to the design system (1-2)',
             'They sometimes contribute to the design system (3)',
@@ -270,7 +281,7 @@ const Section3 = () => (
             ['Well and very well defined (4-5)', [44, 31, 26]],
           ]}
         >
-        <p>How defined is your process that enables design system users to contribute to the design system?” based on “how frequently do design system users contribute to the design system?</p>
+        <p>"How <em>defined</em> is your process that enables design system users to contribute to the design system?” <strong>based on</strong> “how <em>frequently</em> do design system users contribute to the design system?"</p>
       </StackedChart>
       </Figure>
     </GridCell>
@@ -285,7 +296,7 @@ const Section3 = () => (
       <Figure count="3.11" caption="Respondents: 135 | Answers were on a scale of 1 to 5.">
         <StackedChart
           headingLevel="h3"
-          title={<>More defined process is linked to success</>}
+          title="More defined process is linked to success"
           keyMap={[
             'They don’t or rarely contribute to the design system (1-2)',
             'They sometimes contribute to the design system (3)',
@@ -295,11 +306,11 @@ const Section3 = () => (
             ['Not Successful', [33, 33, 33]],
             ['Slightly Successful', [68, 29, 4]],
             ['Moderately Successful', [30, 34, 36]],
-            ['Successful', [432, 36, 32]],
+            ['Successful', [32, 36, 32]],
             ['Very Successful', [10, 50, 40]]
           ]}
         >
-        <p>In your opinion, how successful is your organization’s design system?” based on “how defined is your process that enables design system users to contribute to the design system?</p>
+        <p>"In your opinion, how <em>successful</em> is your organization’s design system?” <strong>based on</strong> “how <em>defined</em> is your process that enables design system users to contribute to the design system?"</p>
       </StackedChart>
       </Figure>
     </GridCell>
@@ -315,7 +326,7 @@ const Section3 = () => (
       <Figure count="3.12" caption="Respondents: 136 | Answers were on a scale of 1 to 5.">
         <BarChart
           headingLevel="h3"
-          title={<>But most systems don’t have a well-defined process</>}
+          title="But most systems don’t have a well-defined process"
           keyMap={['']}
           dataPoints={[
             ['Not define (1)', [17]],
@@ -325,7 +336,7 @@ const Section3 = () => (
             ['Very well defined (5)', [15]],
           ]}
         >
-        <p>How defined is your process that enables design system users to contribute to the design system?</p>
+        <p>"How <em>defined</em> is your process that enables design system users to contribute to the design system?"</p>
       </BarChart>
       </Figure>
     </GridCell>
@@ -352,15 +363,15 @@ const Section3 = () => (
 
     <GridCell>
       <Quote styleFormat="small"
-      cite="-Manager, moderately successful design system">
-        <p>“Nothing can account for the impact to various development stacks in the product. A design system will always produce some technical debt.”</p>
+      cite="Manager, moderately successful design system">
+        <p>“Nothing can account for the impact to various development stacks in the product. <em><strong>A design system will always produce some technical debt</strong></em>.”</p>
       </Quote>
     </GridCell>
 
     <GridCell>
       <Quote styleFormat="small"
-      cite="-Individual contributor, successful design system">
-        <p>“All system choices, especially first-iteration ones made because of resource limitations on the design system team's side impose limitations on products. This limits the evolution of products unless they break free of the system. Future iterations of the system need to support both a future vision as well as a current state, causing a constant deprecated vs not-built-yet state. This interaction can cause deadlock between the system, the platform, and the products.” </p>
+      cite="Individual contributor, successful design system">
+        <p>“All system choices, especially first-iteration ones made because of resource limitations on the design system team's side impose limitations on products. This limits the evolution of products unless they break free of the system. <em><strong>Future iterations of the system need to support both a future vision as well as a current state</strong></em>, causing a constant deprecated vs not-built-yet state. This interaction can cause deadlock between the system, the platform, and the products.”</p>
       </Quote>
     </GridCell>
 
@@ -369,14 +380,14 @@ const Section3 = () => (
         <PieChart
           headingLevel="h3"
           styleFormat="small"
-          title={<>Many teams feel their design system is creating debt</>}
+          title="Many teams feel their design system is creating debt"
           dataPoints={[
             ['It created debt', 53],
             ['It did not create debt', 32],
             ['I dont know', 15]
           ]}
         >
-          <p>Do you feel the way the design system was originally built did or did not create debt for the technical or design departments?</p>
+          <p>"Do you feel the way the design system was originally built did or did not <em>create debt</em> for the technical or design departments?"</p>
         </PieChart>
       </Figure>
     </GridCell>
@@ -394,22 +405,22 @@ const Section3 = () => (
 
     <GridCell>
       <Quote styleFormat="small"
-      cite="-Individual contributor, moderately successful design system">
-        <p>The documentation was incomplete, and thus the proper technical implementation was partial guesswork (guidance not in place)</p>
+      cite="Individual contributor, moderately successful design system">
+        <p>The documentation was incomplete, and thus the proper <em><strong>technical implementation was partial guesswork</strong></em> (guidance not in place)</p>
       </Quote>
     </GridCell>
 
     <GridCell>
       <Quote styleFormat="small"
-      cite="-Individual contributor, slightly successful design system">
-        <p>In an effort to move fast, many projects result in ‘we can incorporate this into the system later’ or ‘we can update this after we ship.’</p>
+      cite="Individual contributor, slightly successful design system">
+        <p>In an effort to move fast, many projects result in ‘we can incorporate this into the system later’ or <em><strong>‘we can update this after we ship.’</strong></em></p>
       </Quote>
     </GridCell>
 
     <GridCell>
       <Quote styleFormat="small"
-      cite="-Manager, slightly successful design system">
-        <p>... we created the design system based on an existing product, there has been debt because it wasn't built from components to start with. However, all features within the product that have been updated over the years are now debt-free (both design and tech). So it's a tricky one to answer. We have a web product which was built from scratch off DS components and that has no debt. I think debt is created/related to when the DS was implemented along the lifecycle of the product/s it's supporting.</p>
+      cite="Manager, slightly successful design system">
+        <p>... we created the design system based on an existing product, there has been debt because it wasn't built from components to start with. However, all features within the product that have been updated over the years are now debt-free (both design and tech). So it's a tricky one to answer. We have a web product which was built from scratch off DS components and that has no debt. I think <em><strong>debt is created/related to when the DS was implemented along the lifecycle of the product/s it's supporting.</strong></em></p>
       </Quote>
     </GridCell>
 
@@ -431,27 +442,27 @@ const Section3 = () => (
             ['Very Successful', [50, 40, 10]]
           ]}
         >
-          <p>In your opinion, how successful is your organization's design system" based on "do you feel the way the design system was originally built did or did not create debt for the technical or design departments?</p>
+          <p>"In your opinion, how <em>successful</em> is your organization's design system" <strong>based on</strong> "do you feel the way the design system was originally built did or did not <em>create debt</em> for the technical or design departments?"</p>
         </StackedChart>
       </Figure>
     </GridCell>
 
     <GridCell>
       <p>Though many respondents thought their design system(s) created debt, this did not correlate to the perception of a less successful design system.</p>
-      <p>How is it that respondents could report debt being a major challenge, but it doesn’t impact perceived success?</p>
+      <p><strong>How is it that respondents could report debt being a major challenge, but it doesn’t impact perceived success?</strong></p>
       <p>One possible explanation could lie in some open-ended answers in which 9 of 42 in-house respondents mentioned that debt seemed unavoidable in an initiative of this size and scope.</p>
     </GridCell>
 
     <GridCell>
       <Quote styleFormat="small"
-      cite="-Individual contributor, successful design system">
+      cite="Individual contributor, successful design system">
         <p>Everything causes tech/design debt. It's unavoidable…Honestly, I feel this is just the curse of software development.</p>
       </Quote>
     </GridCell>
 
     <GridCell>
       <Quote styleFormat="small"
-      cite="-Manager, moderately successful design system">
+      cite="Manager, moderately successful design system">
         <p>How would it not? Any legacy product would need a facelift. Any new product must adhere to guidelines. Yes, there are ways to streamline this, and in some cases the product development process speeds up... A design system will always produce some technical debt.</p>
       </Quote>
     </GridCell>
