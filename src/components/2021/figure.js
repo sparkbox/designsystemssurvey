c68976114c3cbbd9ab40e25ea5a3167e097a8537
aspencer116 <andrew@heysparkbox.com>
2021-06-28 14:16:18 -0400

    feat: Allow figure components to accept CSS classes when used in another file

--- src/components/2021/figure.js
+++ src/components/2021/figure.js
@@ -1,17 +1,21 @@
 import React from "react"
-import PropTypes from "prop-types"
+import {PropTypes} from "prop-types"
 
-const Figure = (props) => (
-  <figure className="cmp-figure">
-    {props.children}
-    <figcaption className="cmp-figure__caption">
-      <span className="cmp-figure__count">
-        <abbr title="Figure">Fig</abbr> {props.count}
-      </span>
-      <span>{props.caption}</span>
-    </figcaption>
-  </figure>
-)
+const Figure = (props) => {
+  let customClasses = (props.className != null) ? ` ${props.className}` : ''
+
+  return (
+    <figure className={`cmp-figure${customClasses}`}>
+      {props.children}
+      <figcaption className="cmp-figure__caption">
+        <span className="cmp-figure__count">
+          <abbr title="Figure">Fig</abbr> {props.count}
+        </span>
+        <span>{props.caption}</span>
+      </figcaption>
+    </figure>
+  )
+}
 
 Figure.propTypes = {
   children: PropTypes.string.isRequired,
