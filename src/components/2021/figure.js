c3280a1529c74ca7168b91f8681b74daa1e20dfb
Colby Allen <colby@heysparkbox.com>
2021-06-30 09:32:33 -0400

    feat: Modifier to swap the figure number and caption

--- src/components/2021/figure.js
+++ src/components/2021/figure.js
@@ -3,13 +3,14 @@ import {PropTypes} from "prop-types"
 
 const Figure = (props) => {
   let customClasses = (props.className != null) ? ` ${props.className}` : ''
+  let directionClass = ` cmp-figure__caption--${props.direction}`
 
   return (
     <figure className={`cmp-figure${customClasses}`}>
       {props.children}
-      <figcaption className="cmp-figure__caption">
+      <figcaption className={`cmp-figure__caption${directionClass}`}>
         <span className="cmp-figure__count">
-          <abbr title="Figure">Fig</abbr> {props.count}
+          <abbr title="Figure">Fig</abbr>&nbsp;{props.count}
         </span>
         <span>{props.caption}</span>
       </figcaption>
@@ -21,6 +22,10 @@ Figure.propTypes = {
   children: PropTypes.string.isRequired,
   count: PropTypes.number.isRequired,
   caption: PropTypes.string
+}
+
+Figure.defaultProps = {
+  direction: "right"
 }
 
 export default Figure
