fee081b9ecbb3ed8673fed04ef700063dba5420d
Colby Allen <colby@heysparkbox.com>
2021-07-01 15:14:16 -0400

    fix: move aria labels to anchor links

--- src/components/2021/table-of-contents.js
+++ src/components/2021/table-of-contents.js
@@ -4,8 +4,8 @@ import SVGArrow from "./svg-arrow"
 
 const TableItem = (props) => {
   return (
-    <AnchorLink className="cmp-toc__link" href={`#section-${props.number}`}>
-      <span className="cmp-toc__section-number" aria-label={`Section ${props.number}`}>0{props.number}</span>
+    <AnchorLink className="cmp-toc__link" href={`#section-${props.number}`} aria-label={`Section ${props.number}: ${props.children}`}>
+      <span className="cmp-toc__section-number">0{props.number}</span>
       <span className="cmp-toc__section-title">{props.children}</span>
       <SVGArrow className="cmp-toc__arrow" />
     </AnchorLink>
