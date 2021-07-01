f3454939a3c84c7ab4aec6aec932e9f7e22d92dd
zastrow <philip@zastrow.co>
2021-06-30 22:15:26 -0400

    feat: IE 11 Support

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -9,23 +9,39 @@ class SEO extends Component {
     super(props);
     this.state = {
       jsEnabled: false,
-      htmlClass: 'no-js'
+      htmlClass: 'no-js',
+      isIE11: false,
     };
     this.htmlClassCheck = this.htmlClassCheck.bind(this);
+    this.ie11Check = this.ie11Check.bind(this);
   }
 
   componentDidMount() {
-    this.setState({ htmlClass : this.htmlClassCheck() })
+    this.setState({ 
+      htmlClass : this.htmlClassCheck(),
+      isIE11 : this.ie11Check()
+    })
   }
 
   htmlClassCheck() {
     let htmlClass = 'safe-focus js';
+    if ( !!window.MSInputMethodContext && !!document.documentMode ) {
+      htmlClass = `${htmlClass} ie11`;
+    }
     if ('IntersectionObserver' in window) {
       htmlClass = `${htmlClass} has-io`;
     } else {
       htmlClass = `${htmlClass} no-io`;
     }
     return htmlClass;
+  }
+  
+  ie11Check() {
+    let isIE11;
+    if ( !!window.MSInputMethodContext && !!document.documentMode ) {
+      isIE11 = '/css/2021-ie.css'
+    }
+    return isIE11;
   }
 
   render () {
@@ -63,7 +79,9 @@ class SEO extends Component {
 
             <link rel="canonical" href={`${data.site.siteMetadata.baseUrl}/${this.props.year}`} />
             {this.props.fontCSS && <link rel="stylesheet" type="text/css" href={this.props.fontCSS} />}
+            {this.state.isIE11 && <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;700&display=swap" rel="stylesheet" />}
             <link rel="stylesheet" type="text/css" href={`/css/${this.props.year}.css`} />
+            {this.state.isIE11 && <link rel="stylesheet" type="text/css" href={this.state.isIE11} />}
           </Helmet>
         )}
       />
