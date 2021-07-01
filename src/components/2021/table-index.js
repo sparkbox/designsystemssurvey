2fc522f3a14ea72b5ac47b8042db965e5374c469
Colby Allen <colby@heysparkbox.com>
2021-07-01 14:38:11 -0400

    fix: wrap table of contents in nav

--- src/components/2021/table-index.js
+++ src/components/2021/table-index.js
@@ -7,24 +7,26 @@ import ArtElement from "../../components/2021/art-element"
 
 const TableOfContents = (props) => {
   return (
-    <div className="cmp-toc util-margin-bottom-20vh" id="table-of-contents">
-      <Grid>
-        <GridCell className="util-margin-right-lg@md util-margin-right-none@lg">
-          <SlimHeading size="full">Jump To</SlimHeading>
-          <TableItem number="1">The Respondents</TableItem>
-          <TableItem number="2">Characteristics &amp; Team</TableItem>
-          <TableItem number="3">Priorities &amp; Challenges</TableItem>
-        </GridCell>
-        <GridCell startMD="5" startLG="7" align="end">
-          <TableItem number="4">Metrics</TableItem>
-          <TableItem number="5">Evolution</TableItem>
-          <TableItem number="6">Conclusion</TableItem>
-        </GridCell>
-        <GridCell startMD="7" rowSpanMD="2" startLG="12" className="util-margin-top-3xl util-display-none util-display-block@lg">
-          <ArtElement type="bars" />
-        </GridCell>
-      </Grid>
-    </div>
+    <nav>
+      <div className="cmp-toc util-margin-bottom-20vh" id="table-of-contents">
+        <Grid>
+          <GridCell className="util-margin-right-lg@md util-margin-right-none@lg">
+            <SlimHeading size="full">Jump To</SlimHeading>
+            <TableItem number="1">The Respondents</TableItem>
+            <TableItem number="2">Characteristics &amp; Team</TableItem>
+            <TableItem number="3">Priorities &amp; Challenges</TableItem>
+          </GridCell>
+          <GridCell startMD="5" startLG="7" align="end">
+            <TableItem number="4">Metrics</TableItem>
+            <TableItem number="5">Evolution</TableItem>
+            <TableItem number="6">Conclusion</TableItem>
+          </GridCell>
+          <GridCell startMD="7" rowSpanMD="2" startLG="12" className="util-margin-top-3xl util-display-none util-display-block@lg">
+            <ArtElement type="bars" />
+          </GridCell>
+        </Grid>
+      </div>
+    </nav>
   )
 }
 
