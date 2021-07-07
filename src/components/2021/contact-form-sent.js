f6071f95d4ef5226ba86b75648fe7c808bbe9ca3
zastrow <philip@zastrow.co>
2021-07-06 22:13:05 -0400

    feat: Add Inline Form Completion Messaging

---
+++ src/components/2021/contact-form-sent.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const ContactFormSent = (props) => {  
+  return (
+    <div class="js-form-success cmp-form__success-message" aria-hidden="true">
+      <h2 class="cmp-type-h2 util-margin-all-none" aria-label="Form Successfully Sent">Sent</h2>
+      <p class="cmp-type-body-default util-margin-top-sm util-margin-bottom-lg">We’re looking forward to learning more together.</p>
+      <hr className="util-hr-solid util-margin-vertical-lg" />
+      <h2 class="cmp-type-section util-margin-all-none">Want to keep exploring right now?</h2>
+      <p class="cmp-type-body-large util-margin-vertical-lg">Answer a few questions to find out where you fall within the Design System Maturity Model and get insights and ideas about how you can take your system to the next level.</p>
+      <a href="/" className="cmp-button cmp-button--inverse">Take Assessment</a>
+    </div>
+  )
+}
+
+export default ContactFormSent
