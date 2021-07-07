b2088d52311e28a2881be841d325ed244cf6a57e
zastrow <philip@zastrow.co>
2021-07-07 14:30:10 -0400

    feat: Add ARIA messaging and form validation

--- src/components/2021/contact-form.js
+++ src/components/2021/contact-form.js
@@ -5,43 +5,42 @@ import ContactFormError from './contact-form-error'
 const ContactForm = (props) => {  
   return (
     <div className="cmp-form">
+      <div class="js-form-aria util-visually-hidden" aria-live="polite" />
       <ContactFormSent />
       <ContactFormError />
       {/* The form attribute will need updated to work as *
          expected and provide the necessary data to MailChimp */}
-      <form className="cmp-form__fieldset" method="post" data-netlify="true" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" novalidate>
+      <form className="cmp-form__fieldset" method="post" data-netlify="true" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form">
         <input type="hidden" name="form-name" value="mc-embedded-subscribe-form" />
   
         <h3 className="cmp-type-body cmp-type-bold util-margin-bottom-md">Can we continue to include you?</h3>
-  
+
+        <div className="js-check-message cmp-form__message util-margin-bottom-md" aria-live="polite" />
+
         <div className="cmp-checkbox">
           <input className="cmp-checkbox__input" type="checkbox" name="checkbox-1" id="checkbox-1" />
-          <div className="cmp-checkbox__label-container">
-            <label className="cmp-checkbox__label" htmlFor="checkbox-1">
-              <span className="cmp-checkbox__label-hidden">262144</span>
-            </label>
-            <span className="cmp-checkbox__faux-label">Yes! I’d like to share more about my system. Let’s set up a time to chat.</span>
-          </div>
+          <label className="cmp-checkbox__label" htmlFor="checkbox-1">
+            Yes! I’d like to share more about my system. Let’s set up a time to chat.
+          </label>
         </div>
   
         <div className="cmp-checkbox">
           <input className="cmp-checkbox__input" type="checkbox" name="checkbox-2" id="checkbox-2" />
-          <div className="cmp-checkbox__label-container">
-            <label className="cmp-checkbox__label" htmlFor="checkbox-2">
-              <span className="cmp-checkbox__label-hidden">65536</span>
-            </label>
-            <span className="cmp-checkbox__faux-label">Sure, I’m always curious! Please send me more design system insights when you can.</span>
-          </div>
+          <label className="cmp-checkbox__label" htmlFor="checkbox-2">
+            Sure, I’m always curious! Please send me more design system insights when you can.
+          </label>
         </div>
   
         <div className="cmp-form-field">
+          <div className="js-name-message cmp-form__message" aria-live="polite" />
           <label className="cmp-type-body-large util-weight-bold" htmlFor="mce-NAME">Name</label>
-          <input className="cmp-form-field__input util-margin-top-sm util-margin-bottom-md" type="text" name="NAME" id="mce-NAME" />
+          <input className="cmp-form-field__input util-margin-top-sm util-margin-bottom-md" type="text" name="NAME" id="mce-NAME" required />
   
+          <div className="js-email-message cmp-form__message" aria-live="polite" />
           <label className="cmp-type-body-large util-weight-bold" htmlFor="mce-EMAIL">Business Email Address</label>
-          <input className="cmp-form-field__input util-margin-top-sm util-margin-bottom-md" type="email" name="EMAIL" id="mce-EMAIL" />
+          <input className="cmp-form-field__input util-margin-top-sm util-margin-bottom-md" type="email" name="EMAIL" id="mce-EMAIL" required />
   
-          <button className="cmp-form-field__button util-margin-top-md" type="submit" name="subscribe" id="mc-embedded-subscribe">Submit</button>
+          <button className="cmp-form-field__button util-margin-top-md" type="submit" name="subscribe" id="mc-embedded-subscribe" formnovalidate="true">Submit</button>
         </div>
      
       </form>
--- src/js/2021/form-submit.js
+++ src/js/2021/form-submit.js
@@ -1,4 +1,5 @@
 function sendFormData() {
+  // Sends the Form Data
   function sendData() {
     let formRequest = new XMLHttpRequest()
     let formsData = new FormData(form)
@@ -6,22 +7,52 @@ function sendFormData() {
       form.parentElement.classList.add('form-success')
       form.setAttribute('aria-hidden', true);
       successMessage.setAttribute('aria-hidden', false);
+      ariaMessage.innerText = 'The form submitted successfully.'
     })
     formRequest.addEventListener('error', function(event) {
       form.parentElement.classList.add('form-error')
       form.setAttribute('aria-hidden', true);
       errorMessage.setAttribute('aria-hidden', false);
+      ariaMessage.innerText = 'There was an error submitting the form. Please refresh the page and try again.'
     })
     formRequest.open('POST', '#')
     formRequest.send(formsData)
   }
+  
+  // Validates the Form Data
+  function validateForm() {
+    let check1 = document.getElementById('checkbox-1');
+    let check2 = document.getElementById('checkbox-2');
+    let name = document.getElementById('mce-NAME');
+    let email = document.getElementById('mce-EMAIL');
+
+    let checkMessage = document.querySelector('.js-check-message');
+    let nameMessage = document.querySelector('.js-name-message');
+    let emailMessage = document.querySelector('.js-email-message');
+    
+    if (!check1.checked && !check2.checked) {
+      checkMessage.innerText = 'One of the contact options must be selected to send the form';
+    }
+    if (name.value === "") {
+      name.classList.add('cmp-form-field__input--error');
+      nameMessage.innerText = 'Your name is required to send the form';
+    }
+    if (email.value === "") {
+      email.classList.add('cmp-form-field__input--error');
+      emailMessage.innerText = 'Your email is required to send the form';
+    }
+    if ((check1.checked || check2.checked) && name.value !== "" && email.value !== "") {
+      sendData()
+    }
+  }
 
   let form = document.querySelector('form')
+  let ariaMessage = document.querySelector('.js-form-aria')
   let errorMessage = document.querySelector('.js-form-error')
   let successMessage = document.querySelector('.js-form-success')
   form.addEventListener('submit', function (e) {
-    e.preventDefault()
-    sendData()
+    e.preventDefault();
+    validateForm();
   })
 }
 
