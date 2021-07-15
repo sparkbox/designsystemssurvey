00b1437d625ede22a021364a90efb8d515749b51
zastrow <philip@zastrow.co>
2021-07-15 16:13:26 -0400

    fix: Add stroke to 404 message

--- src/pages/404.js
+++ src/pages/404.js
@@ -29,7 +29,7 @@ class NotFoundPage extends Component {
             <h1 className="util-visually-hidden">404 Error. Page not found.</h1>
             <h2 className="cmp-error__title">Page Not Found</h2>
        
-            <div className="util-pad-horizontal-lg util-pad-vertical-lg util-pad-vertical-h2@md util-pad-vertical-h3@lg">
+            <div className="cmp-error__content util-pad-horizontal-lg util-pad-vertical-lg util-pad-vertical-h2@md util-pad-vertical-h3@lg">
               <h2 className="cmp-type-h3">We can't seem to find the page you're looking&nbsp;for.</h2>
               <p className="util-font-p-big cmp-type-body-large util-margin-bottom-md">Were you looking for this year's Design Systems Survey results?</p>
               <a className="cmp-type-body-large cmp-error__link" href="/">Take Me to the Latest Results</a>
