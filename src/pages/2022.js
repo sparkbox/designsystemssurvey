4718f468ed87db53568a376a82a2595aac2415f4
zastrow <philip@zastrow.co>
2022-05-09 11:08:00 -0400

    feat: setup initial 2022 styles

--- src/pages/2022.js
+++ src/pages/2022.js
@@ -1,30 +1,28 @@
-import React, { Component } from "react"
+import React from "react"
 import Helmet from "react-helmet"
 import SEO from "../components/SEO"
 
-class Page2022 extends Component {
-  render() {
-    return (
-      <div>
-        <SEO
-          pageDescription="2022 Description"
-          image="ogimage-2022.jpg"
-          year="2022"
+const Page2022 = () => {
+  return (
+    <div>
+      <SEO
+        pageDescription="2022 Description"
+        image="ogimage-2022.jpg"
+        year="2022"
+      />
+
+      <Helmet>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="/css/2022.css"
+          media="all and (-webkit-min-device-pixel-ratio: 0), (-moz-device-pixel-ratio: 0)"
         />
+      </Helmet>
 
-        <Helmet>
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/css/2022.css"
-            media="all and (-webkit-min-device-pixel-ratio: 0), (-moz-device-pixel-ratio: 0)"
-          />
-        </Helmet>
-
-        <h1>Future home of the 2022 Design Systems Survey</h1>
-      </div>
-    )
-  }
+      <h1>Future home of the 2022 Design Systems Survey</h1>
+    </div>
+  )
 }
 
 export default Page2022
