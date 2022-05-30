1fce5c4e19309eee1c6a70d0e4d1774e3d9fc477
zastrow <philip@zastrow.co>
2022-05-30 09:51:05 -0400

    feat: wrap table of content styles

--- src/components/2022/toc-link.js
+++ src/components/2022/toc-link.js
@@ -1,12 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
+import AnchorLink from "react-anchor-link-smooth-scroll"
 
 const TOCLink = ({ to, text }) => {
   return (
-    <a className="cmp-toc-link" href={`#section-${to}`}>
+    <AnchorLink className="cmp-toc-link" href={`#section-${to}`}>
       <span className="cmp-toc-link__number" ariaLabel={`Section ${to}`}>0{to}</span>
       <span className="cmp-toc-link__text">{text}</span>
-    </a>
+      <svg className="cmp-toc-link__arrow" width="24" height="22" fill="none" stroke="currentColor" stroke-width="1.5" xmlns="http://www.w3.org/2000/svg"><path d="M14.768 19.42V1.656H0"/><path d="m22.57 12.213-7.802 7.8-7.8-7.8" /></svg>
+    </AnchorLink>
   )
 }
 
