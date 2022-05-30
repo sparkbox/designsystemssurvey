f7cd50746bb85486a3c37d1fea9994a1bcedf49d
zastrow <philip@zastrow.co>
2022-05-30 16:56:13 -0400

    feat: section headers

---
+++ src/components/2022/section-header.js
@@ -0,0 +1,26 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Grid from "../../components/2022/grid"
+import GridCell from "../../components/2022/grid-cell"
+
+const SectionHeader = ({ title, number, children }) => {
+  let content = (children) ? <GridCell spanMD={7} spanLG={9}>{children}</GridCell> : null
+  return (
+    <header className="section-header">
+      <Grid>
+        <GridCell spanMD={7}>
+          <h2 className="section-header__title" data-number={number}>{title}</h2>
+        </GridCell>
+        {content}
+      </Grid>
+    </header>
+  )
+}
+
+SectionHeader.propTypes = {
+  title: PropTypes.node.isRequired,
+  number: PropTypes.node.isRequired,
+  children: PropTypes.node
+}
+
+export default SectionHeader
