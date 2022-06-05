98673ad1cd848601149684b963f87117f9aa91f4
zastrow <philip@zastrow.co>
2022-06-05 18:29:33 -0400

    feat: wrap number content

--- src/components/2022/gauge.js
+++ src/components/2022/gauge.js
@@ -1,13 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-const Gauge = ({ number, title }) => {
+const Gauge = ({ number, title, size }) => {
   let customProp = {}
   customProp['--gauge-value'] = number ? number : null;
+  const percentClass = size ? 'cmp-gauge__percent cmp-gauge__percent--large' : 'cmp-gauge__percent';
   return (
     <div className="cmp-gauge">
       <div className="cmp-gauge__value" style={customProp}>
-        <div className="cmp-gauge__percent">{number}%</div>
+        <div className={percentClass}>{number}%</div>
       </div>
       <div className="cmp-gauge__title">
         {title}
@@ -19,6 +20,7 @@ const Gauge = ({ number, title }) => {
 Gauge.propTypes = {
   number: PropTypes.node.isRequired,
   title: PropTypes.node.isRequired,
+  size: PropTypes.node,
 }
 
 export default Gauge
--- src/components/2022/related-reading.js
+++ src/components/2022/related-reading.js
@@ -12,8 +12,8 @@ const RelatedReading = ({ image, title, description, href }) => {
           <GridCell spanSM={1} spanMD={2}>
             <img src={image} alt="" className="cmp-related-reading__image" />
           </GridCell>
-          <GridCell spanSM={3} startSM={2} spanMD={5} startMD={3} spanLG={5}>
-            <h4 className="cmp-type-body-xlarge util-margin-all-none util-weight-regular">{title}</h4>
+          <GridCell spanSM={3} startSM={2} spanMD={5} startMD={3} spanLG={6}>
+            <h4 className="cmp-type-body-xlarge util-margin-all-none util-weight-regular">{title}<svg className="cmp-related-reading__arrow" width="14" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.703 6.36H1.058v9.16" stroke="currentColor" stroke-width="1.5"/><path d="m7.383 1.52 4.675 4.838-4.675 4.84" stroke="currentColor" stroke-width="1.5"/></svg></h4>
             <p className="cmp-type-body-default util-margin-all-none">{description}</p>
           </GridCell>
         </Grid>
