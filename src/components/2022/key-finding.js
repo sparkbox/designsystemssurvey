a52acfd3a5f65df1817b8ef2da5367b1ddedbf7f
zastrow <philip@zastrow.co>
2022-06-05 21:35:31 -0400

    feat: key finding block

---
+++ src/components/2022/key-finding.js
@@ -0,0 +1,27 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import Grid from "./grid"
+import GridCell from "./grid-cell"
+
+const KeyFinding = ({ children }) => {
+  return (
+    <div className="cmp-key-finding">
+      <div className="cmp-key-finding__content">
+        <Grid>
+          <GridCell spanLG={10} startLG={2}>
+            <h4 class="cmp-key-finding__title">Key Finding</h4>
+            <p class="cmp-key-finding__text">{ children }</p>
+          </GridCell>
+        </Grid>
+      </div>
+    </div>
+  )
+}
+
+
+KeyFinding.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default KeyFinding
