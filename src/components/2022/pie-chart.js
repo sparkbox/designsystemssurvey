a3e3645e027e9c2c60ce4e9606053eb755791012
zastrow <philip@zastrow.co>
2022-06-11 18:38:55 -0400

    feat: add pie chart

---
+++ src/components/2022/pie-chart.js
@@ -0,0 +1,52 @@
+import React from "react"
+
+const PieChart = () => {
+  let cssProperties = {}
+  cssProperties['--object-1'] = 65
+  cssProperties['--object-2'] = 24
+  cssProperties['--object-3'] = 12
+  return (
+    <div className="cmp-pie-chart" style={cssProperties}>
+      <div className="cmp-pie-chart__diagram" aria-hidden={true}>
+        <div className="cmp-pie-chart__divider cmp-pie-chart__divider--object-1" />
+        <div className="cmp-pie-chart__divider cmp-pie-chart__divider--object-2" />
+        <div className="cmp-pie-chart__divider cmp-pie-chart__divider--object-3" />
+        <div className="cmp-pie-chart__piece cmp-pie-chart__piece--object-1" />
+        <div className="cmp-pie-chart__piece cmp-pie-chart__piece--object-2" />
+        <div className="cmp-pie-chart__piece cmp-pie-chart__piece--object-3" />
+      </div>
+      <div className="cmp-pie-chart__keys">
+        <div className="cmp-pie-chart__key cmp-pie-chart__key--item-1">
+          <span className="cmp-pie-chart__key-title">Agree or Strongly Agree</span>
+          <span className="cmp-pie-chart__key-value">65%</span>
+        </div>
+        <div className="cmp-pie-chart__key cmp-pie-chart__key--item-2">
+          <span className="cmp-pie-chart__key-title">Neither Agree or Disagree</span>
+          <span className="cmp-pie-chart__key-value">24%</span>
+        </div>
+        <div className="cmp-pie-chart__key cmp-pie-chart__key--item-3">
+          <span className="cmp-pie-chart__key-title">Disagree or Strongly Disagree</span>
+          <span className="cmp-pie-chart__key-value">12%</span>
+        </div>
+      </div>
+      <table className="util-visually-hidden">
+        <tbody>
+          <tr>
+            <th>Agree or Strongly Agree</th>
+            <td>65%</td>
+          </tr>
+          <tr>
+            <th>Neither Agree or Disagree</th>
+            <td>24%</td>
+          </tr>
+          <tr>
+            <th>Disagree or Strongly Disagree</th>
+            <td>12%</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default PieChart
