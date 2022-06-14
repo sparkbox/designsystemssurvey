0476258fac89807bdfd0e3abde77bd452589b96c
zastrow <philip@zastrow.co>
2022-06-14 12:58:49 -0400

    fix: add aria-hidden to article iamge

--- src/components/2022/related-article.js
+++ src/components/2022/related-article.js
@@ -11,7 +11,7 @@ const RelatedArticle = ({ image, title, description, href }) => {
           <img src={image} alt="" className="cmp-related-reading__image" />
         </GridCell>
         <GridCell spanSM={4} startSM={3} spanMD={5} startMD={3} spanLG={6}>
-          <h4 className="cmp-type-body-xlarge util-margin-all-none util-weight-regular">{title}<svg className="cmp-related-reading__arrow" width="14" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.703 6.36H1.058v9.16" stroke="currentColor" stroke-width="1.5"/><path d="m7.383 1.52 4.675 4.838-4.675 4.84" stroke="currentColor" stroke-width="1.5"/></svg></h4>
+          <h4 className="cmp-type-body-xlarge util-margin-all-none util-weight-regular">{title}<svg className="cmp-related-reading__arrow" aria-hidden={true} width="14" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.703 6.36H1.058v9.16" stroke="currentColor" stroke-width="1.5"/><path d="m7.383 1.52 4.675 4.838-4.675 4.84" stroke="currentColor" stroke-width="1.5"/></svg></h4>
           <p className="cmp-type-body-default util-margin-all-none">{description}</p>
         </GridCell>
       </Grid>
