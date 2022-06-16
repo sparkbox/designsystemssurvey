62fb921567437c68794f8a4c7de3165f383971a4
zastrow <philip@zastrow.co>
2022-06-16 12:11:20 -0400

    feat: Add 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react"
-import Layout from '../components/2021/layout'
+import Layout from '../components/2022/layout'
 import SEO from '../components/SEO'
 import Helmet from "react-helmet"
+import Circle from '../components/2022/circle'
+import Grid from "../components/2022/grid"
+import GridCell from "../components/2022/grid-cell"
 
 class NotFoundPage extends Component {
   render () {
-    let modernCSS = 'all and (-webkit-min-device-pixel-ratio: 0), (-moz-device-pixel-ratio: 0)';
-    let ieCSS = 'all and (-ms-high-contrast: none), (-ms-high-contrast: active)';
-
     return (
       <Layout>
         <SEO
@@ -16,24 +16,33 @@ class NotFoundPage extends Component {
           year="2022"
         />
         
-      <Helmet>
-        <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;700&display=swap" rel="stylesheet" media={ieCSS} />
-        <link rel="preload" as="font" href="/fonts/2021/Inconsolata.woff2" type="font/woff2" crossorigin="anonymous" media={modernCSS} />
-        <link rel="preload" as="font" href="https://assets.sparkbox.com/fonts/sole/sole-sans-vf-regular-extra-condensed/font.woff2" type="font/woff2" crossorigin="anonymous" media={modernCSS} />
-        <link rel="stylesheet" type="text/css" href="/css/2021.css" media={modernCSS} />
-        <link rel="stylesheet" type="text/css" href="/css/2021-ie.css" media={ieCSS} />
-      </Helmet>
+        <Helmet>
+          <link rel="preload" as="font" href="https://assets.sparkbox.com/fonts/sole/sole-sans-vf-regular-extra-condensed/font.woff2" type="font/woff2" crossorigin="anonymous" />
+          <link rel="preload" as="font" href="https://assets.sparkbox.com/fonts/sole/sole-serif-hairline/font.woff2" type="font/woff2" crossorigin="anonymous" />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="/css/2022.css"
+            media="all and (-webkit-min-device-pixel-ratio: 0), (-moz-device-pixel-ratio: 0)"
+          />
+        </Helmet>
 
-        <div className="obj-lines util-height-full">
-          <div className="cmp-error">
-            <h1 className="util-visually-hidden">404 Error. Page not found.</h1>
-            <h2 className="cmp-error__title">Page Not Found</h2>
-       
-            <div className="cmp-error__content util-pad-horizontal-lg util-pad-vertical-lg util-pad-vertical-h2@md util-pad-vertical-h3@lg">
-              <h2 className="cmp-type-h3">We can't seem to find the page you're looking&nbsp;for.</h2>
+        
+        <Circle />
+        <div className="cmp-hero">
+          <Grid>
+            <GridCell spanSM={4} spanMD={6} startSM={2} spanLG={6} startLG={4}>
+              <h1 className="util-visually-hidden">404 Error. Page not found.</h1>
+              <h2 className="cmp-type-heading-1">Page Not Found</h2>
+            </GridCell>
+            
+            <GridCell spanSM={4} spanMD={6} startSM={2} spanLG={6} startLG={4}>
+              <h3 className="cmp-type-heading-3">We can't seem to find the page you're looking&nbsp;for.</h3>
               <p className="util-font-p-big cmp-type-body-large util-margin-bottom-md">Were you looking for this year's Design Systems Survey results?</p>
-              <a className="cmp-type-body-large cmp-error__link" href="/">Take Me to the Latest Results</a>
-            </div>
+              <a className="cmp-button" href="/">Take Me to the Latest Results</a>
+            </GridCell>
+          </Grid>
+          <div>
           </div>
         </div>
 
