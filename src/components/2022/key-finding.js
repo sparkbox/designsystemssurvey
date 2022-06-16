84487fa7d6bca708e147707406c798c9f22dd700
zastrow <philip@zastrow.co>
2022-06-15 22:27:06 -0400

    feat: animations! animations!

--- src/components/2022/key-finding.js
+++ src/components/2022/key-finding.js
@@ -3,19 +3,22 @@ import PropTypes from "prop-types"
 
 import Grid from "./grid"
 import GridCell from "./grid-cell"
+import Animate from "./animate"
 
 const KeyFinding = ({ children }) => {
   return (
-    <div className="cmp-key-finding">
-      <div className="cmp-key-finding__content">
-        <Grid className="util-margin-hrz-md util-margin-hrz-lg@md util-margin-hrz-none@lg">
-          <GridCell spanMD={8} spanLG={10} startLG={2}>
-            <h3 className="cmp-key-finding__title">Key Finding</h3>
-            <p className="cmp-key-finding__text">{ children }</p>
-          </GridCell>
-        </Grid>
+    <Animate type="height">
+      <div className="cmp-key-finding">
+        <div className="cmp-key-finding__content">
+          <Grid className="util-margin-hrz-md util-margin-hrz-lg@md util-margin-hrz-none@lg">
+            <GridCell spanMD={8} spanLG={10} startLG={2}>
+              <h3 className="cmp-key-finding__title">Key Finding</h3>
+              <p className="cmp-key-finding__text">{ children }</p>
+            </GridCell>
+          </Grid>
+        </div>
       </div>
-    </div>
+    </Animate>
   )
 }
 
